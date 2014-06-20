7345192a5c8b2fe9a1316763d5a4127a9dacbbdb
Jesper van Engelen <contact@jepps.nl>
2014-06-20 10:45:37 +0200

    Formatting

--- classes/column/post/comment-status.php
+++ classes/column/post/comment-status.php
@@ -38,8 +38,10 @@ class CPAC_Column_Post_Comment_Status extends CPAC_Column {
 		$comment_status = $this->get_raw_value( $post_id );
 
 		$value = $this->get_asset_image( 'no.png', $comment_status );
-		if ( 'open' == $comment_status )
+
+		if ( 'open' == $comment_status ) {
 			$value = $this->get_asset_image( 'checkmark.png', $comment_status );
+		}
 
 		return $value;
 	}
