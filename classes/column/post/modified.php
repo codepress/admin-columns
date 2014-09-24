d03447e2429004c63a7dd9fadf08e4699085914e
Jesper van Engelen <contact@jepps.nl>
2014-09-24 12:18:03 +0200

    Add date format option to last modified column

--- classes/column/post/modified.php
+++ classes/column/post/modified.php
@@ -17,27 +17,44 @@ class CPAC_Column_Post_Modified extends CPAC_Column {
 		// Properties
 		$this->properties['type']	 	= 'column-modified';
 		$this->properties['label']	 	= __( 'Last modified', 'cpac' );
+		
+		// Options
+		$this->options['date_format'] = '';
 	}
 
 	/**
 	 * @see CPAC_Column::get_value()
 	 * @since 2.0
 	 */
-	function get_value( $post_id ) {
+	public function get_value( $post_id ) {
 
 		$modified = $this->get_raw_value( $post_id );
 
-		return $this->get_date( $modified ) . ' ' . $this->get_time( $modified );
+		if ( ! $this->options->date_format ) {
+			return $this->get_date( $modified ) . ' ' . $this->get_time( $modified );
+		}
+
+		return date( $this->options->date_format, strtotime( $modified ) );
 	}
 
 	/**
 	 * @see CPAC_Column::get_raw_value()
 	 * @since 2.0.3
 	 */
-	function get_raw_value( $post_id ) {
+	public function get_raw_value( $post_id ) {
 
 		$p = get_post( $post_id );
 
 		return $p->post_modified;
 	}
+
+	/**
+	 * @see CPAC_Column::display_settings()
+	 * @since 2.2.7
+	 */
+	function display_settings() {
+
+		$this->display_field_date_format();
+	}
+
 }
