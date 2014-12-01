1d461145bf16491470249cc8d58e0c2d7a3e254b
Tobias Schutter <tschutter@gmail.com>
2014-12-01 19:31:14 +0100

    set storage models

--- api.php
+++ api.php
@@ -12,6 +12,7 @@ function cpac_set_storage_model_columns( $storage_model, $columns ) {
 
 	global $cpac;
 
+	$cpac->set_storage_models();
 	if ( $storage_model = $cpac->get_storage_model( $storage_model ) ) {
 		$storage_model->set_stored_columns( $columns );
 	}
