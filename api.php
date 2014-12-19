8c0ffea2785acffbb241f88a8ee35228f52dcf19
Tobias Schutter <tschutter@gmail.com>
2014-12-19 22:39:59 +0100

    php export

--- api.php
+++ api.php
@@ -10,9 +10,7 @@
  */
 function cpac_set_storage_model_columns( $storage_model, $columns ) {
 	global $_cac_exported_columns;
-	$_cac_exported_columns = array( $storage_model => $columns );
-
-	add_action( 'cac/loaded', 'cpac_set_exported_columns', 5 );
+	$_cac_exported_columns[ $storage_model ] = $columns;
 }
 
 /**
@@ -22,11 +20,12 @@ function cpac_set_storage_model_columns( $storage_model, $columns ) {
  */
 function cpac_set_exported_columns( $cpac ) {
 	global $_cac_exported_columns;
-
-	$model = key( $_cac_exported_columns );
-	$columns = $_cac_exported_columns[ $model ];
-
-	if ( $storage_model = $cpac->get_storage_model( $model ) ) {
-		$storage_model->set_stored_columns( $columns );
+	if ( $_cac_exported_columns ) {
+		foreach( $_cac_exported_columns as $model => $columns ) {
+			if ( $storage_model = $cpac->get_storage_model( $model ) ) {
+				$storage_model->set_stored_columns( $columns );
+			}
+		}
 	}
 }
+add_action( 'cac/loaded', 'cpac_set_exported_columns', 5 );
