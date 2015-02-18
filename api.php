33f7b6767898505a93620bc17bc6d5c5fc8895de
Tobias Schutter <tschutter@gmail.com>
2015-02-18 14:51:33 +0100

    doc

--- api.php
+++ api.php
@@ -1,5 +1,14 @@
 <?php
 /**
+ * If you like to register a column of your own please have a look at our documentation.
+ * We also have a free start-kit available, which contains all the necessary files.
+ *
+ * Documentation: https://www.admincolumns.com/documentation/developer-docs/creating-new-column-type/
+ * Starter-kit: https://github.com/codepress/cac-column-template/
+ *
+ */
+
+/**
  * Manually set the columns for a storage model
  * This overrides the database settings and thus renders the settings screen for this storage model useless
  *
