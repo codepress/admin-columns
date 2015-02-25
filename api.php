4625a5a10e1c2720bc21eb42add426bfb0e78ea2
Tobias Schutter <tschutter@gmail.com>
2015-02-25 16:06:34 +0100

    API update. ac_register_columns()

--- api.php
+++ api.php
@@ -17,7 +17,21 @@
  * @param string $storage_model Storage model key
  * @param array $columns List of columns ([column_name] => [column_options])
  */
-function cpac_set_storage_model_columns( $storage_model, $columns ) {
+function ac_register_columns( $storage_model, $columns ) {
 	global $_cac_exported_columns;
-	$_cac_exported_columns[ $storage_model ] = $columns;
+
+	$storage_models = (array) $storage_model;
+
+	foreach ( $storage_models as $storage_model ) {
+		if ( isset( $_cac_exported_columns[ $storage_model ] ) ) {
+			$_cac_exported_columns[ $storage_model ] = array_merge( $_cac_exported_columns[ $storage_model ], $columns );
+		}
+		else {
+			$_cac_exported_columns[ $storage_model ] = $columns;
+		}
+	}
+}
+
+function cpac_set_storage_model_columns( $storage_model, $columns ) {
+	ac_register_columns( $storage_model, $columns );
 }
