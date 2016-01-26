4ab0267fd759ab298e8b3eaadaecdcdf866ee521
Tobias Schutter <tschutter@gmail.com>
2016-01-26 14:38:30 +0100

    Shorter syntax

--- classes/storage_model.php
+++ classes/storage_model.php
@@ -686,14 +686,9 @@ abstract class CPAC_Storage_Model {
 
 		$columns = array();
 
-		// get columns
 		if ( ! $this->default_wp_columns ) {
 			$this->default_wp_columns = $this->get_default_columns();
 		}
-
-		$default_columns = $this->default_wp_columns;
-
-		// TODO check if this solves the issue with not displaying value when using "manage_{$post_type}_posts_columns" at CPAC_Storage_Model_Post
 		$registered_columns = $this->get_registered_columns();
 
 		if ( $stored_columns = $this->get_stored_columns() ) {
@@ -727,10 +722,10 @@ abstract class CPAC_Storage_Model {
 
 			// In case of an enabled plugin, we will add that column.
 			// When $diff contains items, it means a default column has not been stored.
-			if ( $diff = array_diff( array_keys( $default_columns ), $this->get_default_stored_columns() ) ) {
+			if ( $diff = array_diff( array_keys( $this->default_wp_columns ), $this->get_default_stored_columns() ) ) {
 				foreach ( $diff as $name ) {
 					// because of the filter "manage_{$post_type}_posts_columns" the columns
-					// that are being added by CPAC will also appear in the $default_columns.
+					// that are being added by CPAC will also appear in the $this->default_wp_columns.
 					// this will filter out those columns.
 					if ( isset( $columns[ $name ] ) ) {
 						continue;
@@ -746,7 +741,7 @@ abstract class CPAC_Storage_Model {
 			}
 		} // When nothing has been saved yet, we return the default WP columns.
 		else {
-			foreach ( array_keys( $default_columns ) as $name ) {
+			foreach ( array_keys( $this->default_wp_columns ) as $name ) {
 				if ( isset( $registered_columns[ $name ] ) ) {
 					$columns[ $name ] = clone $registered_columns[ $name ];
 				}
