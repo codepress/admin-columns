f617ac704f897da8aff5659dc0658e9951ce4f56
Stefan van den Dungen Gronovius <stefan@codepress.nl>
2016-02-01 10:23:38 +0100

    #83 Hotfix created abstract class for init_manage_columns

--- classes/storage_model.php
+++ classes/storage_model.php
@@ -112,6 +112,13 @@ abstract class CPAC_Storage_Model {
 	abstract function get_default_column_names();
 
 	/**
+	 * initialize callback for managing the headers and values for columns
+	 * @since NEWVERSION
+	 *
+	 */
+	abstract function init_manage_columns();
+
+	/**
 	 * @since 2.0
 	 * @return array Column Name | Column Label
 	 */
