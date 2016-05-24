86d86563d3bc0dffe78f3e3d5977ce94d268e477
Stefan van den Dungen Gronovius <stefan@codepress.nl>
2016-05-24 09:23:26 +0200

    format

--- classes/column/post/shortlink.php
+++ classes/column/post/shortlink.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @since NEWVERSION
  */
@@ -9,17 +10,17 @@ class CPAC_Column_Post_Shortlink extends CPAC_Column {
 		parent::init();
 
 		// Properties
-		$this->properties['type']	 = 'column-shortlink';
-		$this->properties['label']	 = __( 'Shortlink', 'codepress-admin-columns' );
+		$this->properties['type'] = 'column-shortlink';
+		$this->properties['label'] = __( 'Shortlink', 'codepress-admin-columns' );
 	}
 
 	function get_value( $post_id ) {
 		$link = $this->get_raw_value( $post_id );
+
 		return '<a href="' . $link . '">' . $link . '</a>';
 	}
 
 	function get_raw_value( $post_id ) {
-
 		return wp_get_shortlink( $post_id );
 	}
 }
