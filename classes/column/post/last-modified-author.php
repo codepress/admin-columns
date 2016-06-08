46a9165db18c7acd1eca221ee22a1904f2fec98d
Tobias Schutter <tschutter@gmail.com>
2016-06-08 16:14:16 +0200

    Cleanup

--- classes/column/post/last-modified-author.php
+++ classes/column/post/last-modified-author.php
@@ -12,16 +12,11 @@ class CPAC_Column_Post_Last_Modified_Author extends CPAC_Column {
 	 * @since 2.2.1
 	 */
 	public function init() {
-
 		parent::init();
 
-		// Properties
 		$this->properties['type']	 			= 'column-last_modified_author';
 		$this->properties['label']	 			= __( 'Last Modified Author', 'codepress-admin-columns' );
 		$this->properties['is_cloneable']		= true;
-
-		// Options
-		$this->options['display_author_as'] = '';
 	}
 
 	/**
@@ -29,9 +24,7 @@ class CPAC_Column_Post_Last_Modified_Author extends CPAC_Column {
 	 * @since 2.0
 	 */
 	public function get_value( $post_id ) {
-
 		$value = '';
-
 		if ( $user_id = $this->get_raw_value( $post_id ) ) {
 			$value = $this->get_display_name( $user_id );
 		}
@@ -54,7 +47,6 @@ class CPAC_Column_Post_Last_Modified_Author extends CPAC_Column {
 	 * @since 2.0
 	 */
 	public function display_settings() {
-
 		$this->display_field_user_format();
 	}
 }
