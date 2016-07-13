0b35d7fd0f9a4cb965d0e9009b368de51a2a1056
Stefan van den Dungen Gronovius <stefan@codepress.nl>
2016-07-13 14:33:47 +0200

    Specific Author Media columns conditional

--- classes/Column/media/Author.php
+++ classes/Column/media/Author.php
@@ -6,4 +6,7 @@ defined( 'ABSPATH' ) or die();
  */
 class AC_Column_Media_Author extends AC_Column_Post_Author {
 
+	public function apply_conditional() {
+		return in_array( $this->get_post_type(), array( 'attachment' ) );
+	}
 }
