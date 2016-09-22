5bad9d032e860e6b030b0d04955c3079aac443d4
Stefan van den Dungen Gronovius <stefan@codepress.nl>
2016-09-22 15:43:16 +0200

    Deprecated fallback methods for old Storage model
    Testes with Media Assistant Library

--- classes/Deprecated/storage_model.php
+++ classes/Deprecated/storage_model.php
@@ -7,8 +7,23 @@ defined( 'ABSPATH' ) or die();
  * @since 2.0
  * @deprecated NEWVERSION
  */
+
+/**
+ * Storage Model
+ *
+ * @since 2.0
+ * @deprecated NEWVERSION
+ */
 abstract class CPAC_Storage_Model extends AC_StorageModel {
 
-	public function init() {}
+	public function init() {
+	}
+
+	public function init_manage_value() {
+	}
+
+	public function add_headings( $columns ) {
+		return $columns;
+	}
 
 }
