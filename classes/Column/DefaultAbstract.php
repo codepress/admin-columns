1672c5a068d0e59262d04ea4e99bcc6d90032119
David Mosterd <david@codepress.nl>
2016-10-26 11:51:20 +0200

    make use of API and fluent

--- classes/Column/DefaultAbstract.php
+++ classes/Column/DefaultAbstract.php
@@ -6,9 +6,9 @@ abstract class AC_Column_DefaultAbstract extends CPAC_Column {
 	public function init() {
 		parent::init();
 
-		$this->properties['type'] = 'column-default';
-		$this->properties['group'] = __( 'Default', 'codepress-admin-columns' );
-		$this->properties['original'] = true;
+		$this->set_property( 'type', 'column-default' )
+		     ->set_property( 'group', __( 'Default', 'codepress-admin-columns' ) )
+		     ->set_property( 'original', true );
 	}
 
 }
--- classes/Column/DefaultUserAbstract.php
+++ classes/Column/DefaultUserAbstract.php
@@ -6,8 +6,8 @@ abstract class AC_Column_DefaultUserAbstract extends CPAC_Column {
 	public function init() {
 		parent::init();
 
-		$this->properties['group'] = __( 'Default', 'codepress-admin-columns' );
-		$this->properties['original'] = true;
+		$this->set_property( 'group', __( 'Default', 'codepress-admin-columns' ) )
+		     ->set_property( 'original', true );
 	}
 
 }
