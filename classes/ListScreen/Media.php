dceacd7c9d65ef7225b1245da37d591c6e816a43
David Mosterd <david@codepress.nl>
2016-12-02 11:14:35 +0100

    set_post_type

--- classes/ListScreen/Media.php
+++ classes/ListScreen/Media.php
@@ -5,17 +5,27 @@ class AC_ListScreen_Media extends AC_ListScreen_Post {
 	public function __construct() {
 		parent::__construct();
 
-		$this->post_type = 'attachment';
-		$this->key = 'wp-media';
 		$this->type = 'media';
 		$this->base = 'upload';
-		$this->screen = 'upload';
 		$this->list_table = 'WP_Media_List_Table';
 		$this->menu_type = __( 'Media' );
 	}
 
 	public function set_manage_value_callback() {
 		add_action( 'manage_media_custom_column', array( $this, 'manage_value' ), 100, 2 );
+	}
+
+	/**
+	 * @param null|string $post_type
+	 *
+	 * return $this;
+	 */
+	public function set_post_type( $post_type = null ) {
+		$this->post_type = 'attachment';
+		$this->key = 'wp-media';
+		$this->screen = 'upload';
+
+		return $this;
 	}
 
 	protected function get_object_by_id( $post_id ) {
--- classes/ListScreen/Post.php
+++ classes/ListScreen/Post.php
@@ -10,7 +10,7 @@ class AC_ListScreen_Post extends AC_ListScreen {
 	public function __construct() {
 		parent::__construct();
 
-		$this->set_post_type( 'post' );
+		$this->set_post_type();
 
 		$this->type = 'post';
 		$this->base = 'edit';
@@ -41,12 +41,20 @@ class AC_ListScreen_Post extends AC_ListScreen {
 	}
 
 	/**
-	 * @param string $post_type
+	 * @param null|string $post_type
+	 *
+	 * @return $this;
 	 */
-	public function set_post_type( $post_type ) {
+	public function set_post_type( $post_type = null ) {
+		if ( null === $post_type ) {
+			$post_type = 'post';
+		}
+
 		$this->post_type = $post_type;
 		$this->key = $post_type;
 		$this->screen = $this->base . '-' . $post_type;
+
+		return $this;
 	}
 
 	/**
