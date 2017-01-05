9d11ed0a202bc0880e0bd16905554fa07e3bcbba
David Mosterd <david@codepress.nl>
2017-01-05 12:21:31 +0100

    meta helper

--- classes/Helper/Meta.php
+++ classes/Helper/Meta.php
@@ -5,12 +5,12 @@ class AC_Helper_Meta {
 	/**
 	 * Returns the properties needed to write custom SQL for the current meta table
 	 *
+	 * @param string $meta_type
+	 *
 	 * @return false|object
 	 */
-	public function get_meta_table_properties( $meta_type = 'post' ) {
+	public function get_meta_table_properties( $meta_type ) {
 		global $wpdb;
-
-		$table = false;
 
 		switch ( $meta_type ) {
 			case 'user':
@@ -28,78 +28,86 @@ class AC_Helper_Meta {
 				$id_column = 'ID';
 
 				break;
-		}
-
-		if ( ! $table || ! $id_column ) {
-			return false;
+			default:
+				return false;
 		}
 
 		// setup meta tables
-		$q = new WP_Meta_Query();
-		$q->get_sql( $meta_type, $table, $id_column );
+		$query = new WP_Meta_Query();
+		$query->get_sql( $meta_type, $table, $id_column );
 
 		return (object) array(
 			'table'      => $table,
 			'id'         => $id_column,
-			'meta_id'    => $q->meta_id_column,
-			'meta_table' => $q->meta_table,
+			'meta_id'    => $query->meta_id_column,
+			'meta_table' => $query->meta_table,
 		);
 	}
 
-	private function get_query( $meta_type, $table, $id_column, $key, array $in ) {
-		global $wpdb;
-
-		// setup meta tables
-		$q = new WP_Meta_Query();
-		$q->get_sql( $meta_type, $table, $id_column );
-
-		// escape integers before imploding
-		array_walk( $in, 'intval' );
-
-		$in = implode( ', ', $in );
-
-		$sql = "
-			SELECT t.$id_column, m.meta_value
-			FROM $table AS t
-			LEFT JOIN $q->meta_table AS m ON m.$q->meta_id_column = t.$id_column 
-				AND m.meta_key = %s
-			WHERE t.$id_column IN ( $in )
-			ORDER BY m.meta_value
-		";
-
-		return $wpdb->prepare( $sql, $key );
-	}
-
 	/**
+	 * Retrieve meta value per id
+	 *
+	 * @todo handle identical meta_keys per id
 	 * @return array
 	 */
-	public function get_values_by_ids( $ids, $meta_key, $meta_type = 'post' ) {
+	public function get_values_by_ids( $ids, $meta_key, array $args = array() ) {
 		global $wpdb;
 
-		switch ( $meta_type ) {
-			case 'user':
-				$table = $wpdb->users;
-				$id_column = 'ID';
+		$defaults = array(
+			'meta_type' => 'post',
+			'orderby'   => null,
+			'order'     => 'ASC',
+			'data_type' => null,
+		);
 
-				break;
-			case 'comment':
-				$table = $wpdb->comments;
-				$id_column = 'comment_ID';
+		$args = (object) array_merge( $defaults, $args );
 
-				break;
-			default:
-				$table = $wpdb->posts;
-				$id_column = 'ID';
+		// sanitize args
+		if ( $args->order !== 'ASC' || $args->order !== 'DESC' ) {
+			$args->order = $defaults['order'];
 		}
 
-		$sql = $this->get_query( $meta_type, $table, $id_column, $meta_key, $ids );
-		$r = $wpdb->get_results( $sql, ARRAY_A );
+		if ( ! in_array( $args->orderby, array( 'meta_value', 'meta_key', 'id' ) ) ) {
+			$args->orderby = $defaults['orderby'];
+		}
+
+		$properties = $this->get_meta_table_properties( $args->meta_type );
+
+		if ( ! $properties ) {
+			return array();
+		}
+
+		// sanitize ids
+		array_walk( $ids, 'intval' );
+
+		$in = implode( ', ', $ids );
+
+		$sql = "
+			SELECT $properties->meta_id, meta_value
+			FROM $properties->meta_table
+			WHERE meta_key = %s
+			AND $properties->meta_id IN ( $in )
+		";
+
+		if ( $args->orderby ) {
+			if ( 'id' === $args->orderby ) {
+				$args->orderby = $properties->meta_id;
+			}
+
+			$sql .= "ORDER BY $args->orderby $args->order";
+		}
+
+		$results = $wpdb->get_results( $wpdb->prepare( $sql, $meta_key ), ARRAY_A );
 
 		$values = array();
 
-		if ( is_array( $r ) ) {
-			foreach ( $r as $row ) {
-				$values[ $row['ID'] ] = maybe_unserialize( $row['meta_value'] );
+		if ( is_array( $results ) ) {
+			foreach ( $results as $row ) {
+				if ( ! $args->data_type ) {
+					$row['meta_value'] = maybe_unserialize( $row['meta_value'] );
+				}
+
+				$values[ $row[ $properties->meta_id ] ] = $row['meta_value'];
 			}
 		}
 
