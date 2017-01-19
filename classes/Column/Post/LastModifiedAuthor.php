931af6f8a7caeb2a3d438265306a243ba830b5ee
David Mosterd <david@codepress.nl>
2017-01-19 10:17:20 +0100

    meta columns for filtering

--- classes/Column/Post/LastModifiedAuthor.php
+++ classes/Column/Post/LastModifiedAuthor.php
@@ -6,15 +6,15 @@
  *
  * @since 2.0
  */
-class AC_Column_Post_LastModifiedAuthor extends AC_Column {
+class AC_Column_Post_LastModifiedAuthor extends AC_Column_Meta {
 
 	public function __construct() {
 		$this->set_type( 'column-last_modified_author' );
 		$this->set_label( __( 'Last Modified Author', 'codepress-admin-columns' ) );
 	}
 
-	public function get_raw_value( $post_id ) {
-		return get_post_meta( $post_id, '_edit_last', true );
+	public function get_meta_key() {
+		return '_edit_last';
 	}
 
 	public function register_settings() {
