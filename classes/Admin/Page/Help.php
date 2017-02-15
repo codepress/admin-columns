c92bcd984c956efa2411b5c6d7434164d77257ce
Stefan van den Dungen Gronovius <stefan@codepress.nl>
2017-02-15 17:14:07 +0100

    Complete list of filters added

--- classes/Admin/Page/Help.php
+++ classes/Admin/Page/Help.php
@@ -11,7 +11,7 @@ class AC_Admin_Page_Help extends AC_Admin_Page {
 			->set_slug( 'help' )
 			->set_label( __( 'Help', 'codepress-admin-columns' ) );
 
-		add_action( 'admin_init', array( $this, 'init' ) );
+
 	}
 
 	/**
@@ -22,17 +22,36 @@ class AC_Admin_Page_Help extends AC_Admin_Page {
 
 	}
 
-	public function init() {
+	public function init_deprecated_filters() {
+	    if( ! AC()->admin()->is_admin_screen() ){
+	        return;
+        }
 		$types = array( 'post', 'user', 'comment', 'link', 'media' );
 		$post_types = get_post_types();
 		$columns = array();
+
 		foreach ( AC()->get_list_screens() as $ls ) {
-			foreach ( $ls->get_column_types() as $column ) {
+            foreach ( $ls->get_column_types() as $column ) {
 				$columns[ $column->get_type() ] = $column->get_type();
 			}
 		}
 
 		$this->deprecated_filter( 'cac/headings/label', 'NEWVERSION', 'cac-columns-custom' );
+		$this->deprecated_filter( 'cac/column/meta/value', 'NEWVERSION', 'cac-column-meta-value' );
+		$this->deprecated_filter( 'cac/column/meta/types', 'NEWVERSION', 'cac-column-meta-types' );
+		$this->deprecated_filter( 'cac/settings/tabs', 'NEWVERSION', 'filter-reference/cac-settings-tabs' );
+		$this->deprecated_filter( 'cac/editable/is_column_editable', 'NEWVERSION', 'cac-editable-is_column_editable' );
+		$this->deprecated_filter( 'cac/editable/editables_data', 'NEWVERSION', 'cac-editable-editables_data' );
+		$this->deprecated_filter( 'cac/editable/options', 'NEWVERSION', 'cac-editable-editables_data' );
+		$this->deprecated_filter( 'cac/inline-edit/ajax-column-save/value', 'NEWVERSION', 'cac-inline-edit-ajax-column-save-value' );
+		$this->deprecated_filter( 'cac/addon/filtering/options', 'NEWVERSION', 'cac-addon-filtering-options' );
+		$this->deprecated_filter( 'cac/addon/filtering/dropdown_top_label', 'NEWVERSION', 'cac-addon-filtering-dropdown_top_label' );
+		$this->deprecated_filter( 'cac/addon/filtering/taxonomy/terms_args', 'NEWVERSION', 'cac-addon-filtering-taxonomy-terms_args' );
+		$this->deprecated_filter( 'cac/addon/filtering/dropdown_empty_option', 'NEWVERSION', 'cac-addon-filtering-taxonomy-terms_args' );
+		$this->deprecated_filter( 'cac/column/actions/action_links', 'NEWVERSION', 'cac-column_actions-action_links' );
+		$this->deprecated_filter( 'cac/acf/format_acf_value', 'NEWVERSION' );
+
+
 		$this->deprecated_filter( 'cac/columns/custom', 'NEWVERSION', 'cac-columns-custom' );
 		foreach ( $types as $type ) {
 			$this->deprecated_filter( 'cac/columns/custom/type=' . $type, 'NEWVERSION', 'cac-columns-custom' );
@@ -47,20 +66,15 @@ class AC_Admin_Page_Help extends AC_Admin_Page {
 			$this->deprecated_filter( 'cac/column/value/' . $type, 'NEWVERSION', 'cac-column-value' );
 		}
 
-		$this->deprecated_filter( 'cac/column/meta/value', 'NEWVERSION', 'cac-column-meta-value' );
-		$this->deprecated_filter( 'cac/column/meta/types', 'NEWVERSION', 'cac-column-meta-types' );
+
 		$this->deprecated_filter( 'cac/columns/custom', 'NEWVERSION', 'cac-columns-custom' );
 		foreach ( $types as $type ) {
 			$this->deprecated_filter( 'cac/columns/custom/type=' . $type, 'NEWVERSION', 'cac-columns-custom' );
 		}
+
 		foreach ( $post_types as $post_type ) {
 			$this->deprecated_filter( 'cac/columns/custom/post_type=' . $post_type, 'NEWVERSION', 'cac-columns-custom' );
 		}
-
-		$this->deprecated_filter( 'cac/settings/tabs', 'NEWVERSION', 'filter-reference/cac-settings-tabs' );
-		$this->deprecated_filter( 'cac/editable/is_column_editable', 'NEWVERSION', 'cac-editable-is_column_editable' );
-		$this->deprecated_filter( 'cac/editable/editables_data', 'NEWVERSION', 'cac-editable-editables_data' );
-		$this->deprecated_filter( 'cac/editable/options', 'NEWVERSION', 'cac-editable-editables_data' );
 
 		$this->deprecated_filter( 'cac/editable/column_value', 'NEWVERSION', 'cac-editable-column_value' );
 		foreach ( $columns as $column_type ) {
@@ -75,7 +89,10 @@ class AC_Admin_Page_Help extends AC_Admin_Page {
 
 	}
 
-	public function display() { ?>
+	public function display() {
+
+		$this->init_deprecated_filters();
+	    ?>
         <h2><?php _e( 'Help', 'codepress-admin-columns' ); ?></h2>
 
         <h3>Deprecated Filters</h3>
