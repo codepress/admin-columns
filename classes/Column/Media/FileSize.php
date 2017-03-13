eb7a257a95d478d7ef3502788f6fa59420314cdf
Tobias Schutter <tschutter@gmail.com>
2017-03-13 16:28:12 +0100

    File helper

--- classes/Column/Media/FileSize.php
+++ classes/Column/Media/FileSize.php
@@ -17,27 +17,10 @@ class AC_Column_Media_FileSize extends AC_Column {
 		$abs = str_replace( WP_CONTENT_URL, WP_CONTENT_DIR, $file );
 
 		if ( file_exists( $abs ) ) {
-			$value = $this->get_readable_filesize( filesize( $abs ) );
+			$value = ac_helper()->file->get_readable_filesize( filesize( $abs ) );
 		}
 
 		return $value;
-	}
-
-	/**
-	 * Convert file size to readable format
-	 *
-	 * @since 1.4.5
-	 *
-	 * @param string $size
-	 *
-	 * @return string Readable file size
-	 */
-	public function get_readable_filesize( $size ) {
-		$filesize_units = array( 'Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB' );
-
-		$i = (int) floor( log( $size, 1024 ) );
-
-		return $size ? round( $size / pow( 1024, $i ), 2 ) . ' ' . $filesize_units[ $i ] : '0 Bytes';
 	}
 
 }
---
+++ classes/Helper/File.php
@@ -0,0 +1,27 @@
+<?php
+
+class AC_Helper_File {
+
+	/**
+	 * Convert file size to readable format
+	 *
+	 * @since 1.4.5
+	 *
+	 * @param string $size Size in bytes
+	 * @param int    $decimals
+	 *
+	 * @return string|false Readable file size
+	 */
+	public function get_readable_filesize( $bytes, $decimals = 2 ) {
+		if ( ! $bytes ) {
+			return false;
+		}
+
+		$filesize_units = array( 'Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB' );
+
+		$i = (int) floor( log( $bytes, 1024 ) );
+
+		return round( $bytes / pow( 1024, $i ), $decimals ) . ' ' . $filesize_units[ $i ];
+	}
+
+}
