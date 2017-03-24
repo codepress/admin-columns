53f5900d4f53428a0176d806355e4c515ea9495a
David Mosterd <david@codepress.nl>
2017-03-24 09:37:54 +0100

    version bump

--- codepress-admin-columns.php
+++ codepress-admin-columns.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Admin Columns
-Version: 2.6beta
+Version: 3.0beta
 Description: Customize columns on the administration screens for post(types), pages, media, comments, links and users with an easy to use drag-and-drop interface.
 Author: AdminColumns.com
 Author URI: https://www.admincolumns.com
