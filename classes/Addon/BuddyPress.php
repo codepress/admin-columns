ef52ef6f6b2d3b3c595cc772669c60606d72eba1
Tobias Schutter <tschutter@gmail.com>
2017-06-07 17:04:32 +0200

    BuddyPress addon

---
+++ classes/Addon/BuddyPress.php
@@ -0,0 +1,26 @@
+<?php
+
+class AC_Addon_BuddyPress extends AC_Addon {
+
+	public function __construct() {
+		parent::__construct( 'ac-addon-buddypress' );
+
+		$this
+			->set_title( __( 'BuddyPress', 'codepress-admin-columns' ) )
+			->set_logo( AC()->get_plugin_url() . 'assets/images/addons/buddypress.png' )
+			->set_icon( $this->get_logo() )
+			->set_link( ac_get_site_utm_url( 'buddypress-columns', 'addon', 'buddypress' ) )
+			->set_description( __( 'Display any your Profile Fields for BuddyPress on your users overview.', 'codepress-admin-columns' ) )
+			->add_plugin( 'buddypress' );
+	}
+
+	public function display_promo() {
+		echo $this->get_title() . ' ';
+		$this->display_icon();
+	}
+
+	public function is_plugin_active() {
+		return class_exists( 'BuddyPress', false );
+	}
+
+}
