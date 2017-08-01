af6f6d0db8cf55e3acc3bcc4d35104eb2cb3bbc9
Tobias Schutter <tschutter@gmail.com>
2017-08-01 13:32:58 +0200

    Update tax label when selecting. Rename allow comments.

--- classes/Settings/Column/Taxonomy.php
+++ classes/Settings/Column/Taxonomy.php
@@ -18,6 +18,7 @@ class AC_Settings_Column_Taxonomy extends AC_Settings_Column {
 		$taxonomy = $this->create_element( 'select', 'taxonomy' );
 		$taxonomy->set_no_result( __( 'No taxonomies available.' ) )
 		         ->set_options( ac_helper()->taxonomy->get_taxonomy_selection_options( $this->column->get_post_type() ) )
+		         ->set_attribute( 'data-label', 'update' )
 		         ->set_attribute( 'data-refresh', 'column' );
 
 		return new AC_View( array(
