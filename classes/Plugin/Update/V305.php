09c8b1ba336b4ac9a68e9fe4841ed9f82b812f6d
Tobias Schutter <tschutter@gmail.com>
2017-11-08 13:00:47 +0100

    update 305

---
+++ classes/Plugin/Update/V305.php
@@ -0,0 +1,122 @@
+<?php
+
+class ACP_Plugin_Update_V305 extends AC_Plugin_Update {
+
+	/**
+	 * @return bool
+	 */
+	public function needs_update() {
+		return $this->is_less_or_equal_stored_version();
+	}
+
+	public function apply_update() {
+		$this->migrate_user_specific_settings();
+		$this->delete_deprecated_settings();
+		$this->update_stored_version();
+	}
+
+	protected function set_version() {
+		$this->version = '3.0.5';
+	}
+
+	/**
+	 * @param string $key
+	 *
+	 * @return bool
+	 */
+	private function validate_key( $key ) {
+		if ( empty( $key ) ) {
+			return false;
+		}
+
+		if ( ! is_string( $key ) ) {
+			return false;
+		}
+
+		if ( strlen( $key ) < 10 ) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * @param string $key
+	 *
+	 * @return array
+	 */
+	private function get_meta( $key ) {
+		global $wpdb;
+
+		if ( ! $this->validate_key( $key ) ) {
+			return array();
+		}
+
+		$sql = $wpdb->prepare( "
+			SELECT *
+			FROM {$wpdb->usermeta}
+			WHERE meta_key LIKE %s
+			ORDER BY user_id ASC
+		", $key );
+
+		$results = $wpdb->get_results( $sql );
+
+		if ( ! $results ) {
+			return array();
+		}
+
+		return $results;
+	}
+
+	/**
+	 * Migrate USER specific preferences
+	 */
+	private function migrate_user_specific_settings() {
+		global $wpdb;
+
+		$mapping = array(
+			'cpac-hide-install-addons-notice' => 'ac_hide_notice_addons',
+			'cpac-hide-review-notice'         => 'ac_hide_notice_review',
+		);
+
+		foreach ( $mapping as $current => $new ) {
+			$sql_meta_key = $wpdb->esc_like( $current ) . '%';
+
+			foreach ( $this->get_meta( $sql_meta_key ) as $row ) {
+				update_user_meta( $row->user_id, $new, $row->meta_value );
+			}
+
+			$this->delete( $current );
+		}
+	}
+
+	/**
+	 * Preference to be REMOVED
+	 */
+	private function delete_deprecated_settings() {
+		$this->delete( 'cpac_current_model' );
+		$this->delete( 'cpac-install-timestamp' );
+	}
+
+	/**
+	 * Remove meta data
+	 *
+	 * @param string $key
+	 */
+	private function delete( $key ) {
+		global $wpdb;
+
+		if ( ! $this->validate_key( $key ) ) {
+			return;
+		}
+
+		$sql = $wpdb->prepare( "
+				DELETE
+				FROM {$wpdb->usermeta}
+				WHERE meta_key LIKE %s
+			", $wpdb->esc_like( $key ) . '%' );
+
+		$wpdb->query( $sql );
+	}
+
+}
