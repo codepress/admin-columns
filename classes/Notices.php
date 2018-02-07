98587042651decb5721ab5bd85178418332dd82f
Tobias Schutter <tschutter@gmail.com>
2018-02-07 15:07:57 +0100

    hooks and styles

--- classes/Notices.php
+++ classes/Notices.php
@@ -5,31 +5,35 @@ class AC_Notices {
 	/**
 	 * @var AC_Notice[]
 	 */
-	private $notices;
+	private $notices = array();
 
 	public function __construct() {
+		add_action( 'admin_init', array( $this, 'hooks' ), 20 ); // make sure to register notices on `admin_init` before prio 20
+		add_action( 'admin_enqueue_scripts', array( $this, 'scripts' ) );
+
 		add_action( 'admin_notices', array( $this, 'display' ) );
 		add_action( 'network_admin_notices', array( $this, 'display' ) );
+	}
+
+	public function hooks() {
+		foreach ( $this->notices as $notice ) {
+			$notice->register();
+		}
+	}
+
+	public function scripts() {
+		foreach ( $this->notices as $notice ) {
+			$notice->scripts();
+		}
 	}
 
 	/**
 	 * Display admin notice
 	 */
 	public function display() {
-		if ( ! $this->notices ) {
-			return;
-		}
-
 		$messages = array();
 
-		foreach ( (array) $this->notices as $notice ) {
-
-			// Load scripts & styles
-			$notice->scripts();
-
-			// Load hooks
-			$notice->register();
-
+		foreach ( $this->notices as $notice ) {
 			$messages[] = $notice->render();
 		}
 
