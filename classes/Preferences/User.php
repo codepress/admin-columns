80e65a579143a67a0300259c9737ceae06c33dd1
Tobias Schutter <tschutter@gmail.com>
2018-03-13 13:56:33 +0100

    private vars

--- classes/Preferences/User.php
+++ classes/Preferences/User.php
@@ -6,14 +6,14 @@ class AC_Preferences_User extends AC_Preferences {
 	 * @return bool
 	 */
 	public function save() {
-		return (bool) update_user_meta( $this->user_id, $this->get_key(), $this->data );
+		return (bool) update_user_meta( $this->get_user_id(), $this->get_key(), $this->data );
 	}
 
 	/**
 	 * @return false|array
 	 */
 	protected function load() {
-		return get_user_meta( $this->user_id, $this->get_key(), true );
+		return get_user_meta( $this->get_user_id(), $this->get_key(), true );
 	}
 
 }
