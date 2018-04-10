f6cb9e43bf7ec30d3d526544033afc5295ff07bb
Tobias Schutter <tschutter@gmail.com>
2018-04-10 17:10:43 +0200

    id

--- templates/message/notice.php
+++ templates/message/notice.php
@@ -1,4 +1,4 @@
-<div class="ac-notice notice <?php echo esc_attr( $this->type ); ?>">
+<div class="ac-notice notice <?php echo esc_attr( $this->type . ' ' . $this->id ); ?>">
 	<div class="ac-notice__body">
 		<?php echo $this->message; ?>
 	</div>
