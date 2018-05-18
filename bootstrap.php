8a1db4bed5bb84f84c1c74bca1da34fb69c47aa0
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2018-05-18 15:18:00 +0200

    Added aliases

--- bootstrap.php
+++ bootstrap.php
@@ -8,7 +8,9 @@ require_once __DIR__ . '/api.php';
 require_once __DIR__ . '/classes/Autoloader.php';
 
 AC\Autoloader::instance()->register_prefix( 'AC', __DIR__ . '/classes' );
-AC\Autoloader\Underscore::instance()->add_alias( 'AC\ListScreen', 'AC_ListScreen' );
+AC\Autoloader\Underscore::instance()
+	->add_alias( 'AC\ListScreen', 'AC_ListScreen' )
+	->add_alias( 'AC\Settings\FormatValue', 'AC_Settings_FormatValueInterface' );
 
 /**
  * For loading external resources, e.g. column settings.
