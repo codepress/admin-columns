f297a9f6d51eb9ee018a96fe07d5741084a361f1
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2018-07-25 09:22:45 +0200

    Short delay for refresh

--- src/js-new/admin/columns/events/refresh.js
+++ src/js-new/admin/columns/events/refresh.js
@@ -2,13 +2,16 @@ let refresh = function( column ) {
 	column.$el.find( '[data-refresh="column"]' ).on( 'change', function() {
 		// Allow plugins to hook into this event
 		$( document ).trigger( 'pre_column_refresh', column.$el );
-
 		column.$el.addClass( 'loading' );
-		column.refresh().always( function() {
-			column.$el.removeClass( 'loading' );
+		
+		setTimeout( function() {
+			column.refresh().always( function() {
+				column.$el.removeClass( 'loading' );
 
-			$( document ).trigger( 'AC.columnRefresh', { column } );
-		} );
+				$( document ).trigger( 'AC.columnRefresh', { column } );
+			} );
+		}, 200 );
+
 	} );
 };
 
