e4c45f0fe3d03ac8896dccb0fda815d0e6d71133
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2018-08-14 11:19:32 +0200

    Check for system files to prevent fatal errors
    https://github.com/codepress/admin-columns-issues/issues/1119

--- classes/Autoloader.php
+++ classes/Autoloader.php
@@ -11,7 +11,6 @@ class Autoloader {
 
 	/**
 	 * Register prefixes and their path
-	 *
 	 * @var string[]
 	 */
 	protected $prefixes;
@@ -134,6 +133,11 @@ class Autoloader {
 
 		/* @var \DirectoryIterator $leaf */
 		foreach ( $iterator as $leaf ) {
+			// Check for system files
+			if ( 0 === strpos( $leaf->getBasename(), '._' ) ) {
+				continue;
+			}
+
 			if ( 'php' === $leaf->getExtension() ) {
 				$classes[] = $namespace . '\\' . pathinfo( $leaf->getBasename(), PATHINFO_FILENAME );
 			}
