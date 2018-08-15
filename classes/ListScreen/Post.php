3eb31533908a1efc250741a2a46fdafd4f9740fe
Tobias Schutter <tschutter@gmail.com>
2018-08-15 11:44:35 +0200

    Optimise imports

--- classes/ListScreen/Post.php
+++ classes/ListScreen/Post.php
@@ -3,6 +3,7 @@
 namespace AC\ListScreen;
 
 use AC\ListScreenPost;
+use WP_Posts_List_Table;
 
 class Post extends ListScreenPost {
 
@@ -23,12 +24,12 @@ class Post extends ListScreenPost {
 	}
 
 	/**
-	 * @return \WP_Posts_List_Table
+	 * @return WP_Posts_List_Table
 	 */
 	protected function get_list_table() {
 		require_once( ABSPATH . 'wp-admin/includes/class-wp-posts-list-table.php' );
 
-		return new \WP_Posts_List_Table( array( 'screen' => $this->get_screen_id() ) );
+		return new WP_Posts_List_Table( array( 'screen' => $this->get_screen_id() ) );
 	}
 
 	/**
