6912bbe1590a5402ba92e4650cc6513c863e9dd1
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2018-08-17 09:25:30 +0200

    Better Modal close fix

--- src/js/admin/columns/modal.js
+++ src/js/admin/columns/modal.js
@@ -8,22 +8,16 @@ class Modal {
 
 	static initGlobalEvents() {
 
-		let buttons = document.querySelectorAll( '[data-ac-open-modal]' );
-		if ( buttons.length ) {
-			buttons.forEach( ( button ) => {
+		$( document ).on( 'click', '[data-ac-open-modal]', function( e ) {
+			e.preventDefault();
+			let target = e.target.dataset.acOpenModal;
+			let el = document.querySelector( target );
 
-				button.addEventListener( 'click', ( e ) => {
-					e.preventDefault();
-					let target = e.target.dataset.acOpenModal;
-					let el = document.querySelector( target );
+			if ( el && el.AC_MODAL ) {
+				el.AC_MODAL.open();
+			}
+		} );
 
-					if ( el && el.AC_MODAL ) {
-						el.AC_MODAL.open();
-					}
-				} );
-
-			} );
-		}
 	}
 
 	initEvents() {
@@ -41,7 +35,7 @@ class Modal {
 			}
 		} );
 
-		let dismissButtons = this.el.querySelectorAll( '[data-dismiss="modal"]' );
+		let dismissButtons = this.el.querySelectorAll( '[data-dismiss="modal"], .ac-modal__dialog__close' );
 		if ( dismissButtons.length > 0 ) {
 			dismissButtons.forEach( ( b ) => {
 				b.addEventListener( 'click', ( e ) => {
