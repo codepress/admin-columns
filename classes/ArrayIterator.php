03becbf463e1f3bdf9da994b90d923bef1108a3f
Zaas <david@codepress.nl>
2018-09-05 15:41:55 +0200

    iterator

--- classes/ArrayIterator.php
+++ classes/ArrayIterator.php
@@ -52,4 +52,37 @@ abstract class ArrayIterator
 		return reset( $this->array );
 	}
 
+	/**
+	 * @param $key
+	 *
+	 * @return false|mixed
+	 */
+	public function get_offset( $offset ) {
+		if ( ! $this->has_offset( $offset ) ) {
+			return false;
+		}
+
+		return $this->array[ $offset ];
+	}
+
+	/**
+	 * @param $key
+	 *
+	 * @return bool
+	 */
+	public function has_offset( $offset ) {
+		return array_key_exists( $offset, $this->array );
+	}
+
+	/**
+	 * @return array
+	 */
+	public function get_copy() {
+		$copy = $this->array;
+
+		reset( $copy );
+
+		return $copy;
+	}
+
 }
