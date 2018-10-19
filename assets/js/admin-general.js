0738d607b242d367184308058f7fd0f8fba8d3a9
Tobias Schutter <tschutter@gmail.com>
2018-10-19 16:17:13 +0200

    pointer

--- assets/js/admin-general.js
+++ assets/js/admin-general.js
@@ -101,7 +101,7 @@ jQuery(document).ready(function ($) {
     return false;
   }
 
-  ac_pointer($);
+  ac_pointers($);
   ac_help($);
 });
 /*
@@ -109,81 +109,85 @@ jQuery(document).ready(function ($) {
  *
  */
 
-function ac_pointer($) {
+function ac_pointers($) {
   $('.ac-pointer').each(function () {
-    // vars
-    var el = $(this),
-        html = el.attr('rel'),
-        pos = el.attr('data-pos'),
-        w = el.attr('data-width'),
-        noclick = el.attr('data-noclick');
-    var position = {
-      at: 'left top',
-      // position of wp-pointer relative to the element which triggers the pointer event
-      my: 'right top',
-      // position of wp-pointer relative to the at-coordinates
-      edge: 'right' // position of arrow
-
-    };
-    var width = w ? w : 250;
-
-    if ('right' === pos) {
-      position = {
-        at: 'right middle',
-        my: 'left middle',
-        edge: 'left'
-      };
-    }
-
-    if ('left' === pos) {
-      position = {
-        at: 'left middle',
-        my: 'right middle',
-        edge: 'right'
-      };
-    } // create pointer
-
-
-    el.pointer({
-      content: $('#' + html).html(),
-      position: position,
-      pointerWidth: width,
-      // bug fix. with an arrow on the right side the position of wp-pointer is incorrect. it does not take
-      // into account the padding of the arrow. adding "wp-pointer-' + position.edge"  will fix that.
-      pointerClass: 'ac-wp-pointer wp-pointer wp-pointer-' + position.edge + (noclick ? ' noclick' : '')
-    }); // click
-
-    if (!noclick) {
-      el.click(function () {
-        if (el.hasClass('open')) {
-          el.removeClass('open');
-        } else {
-          el.addClass('open');
-        }
-      });
-    } // show on hover
-
-
-    el.hover(function () {
-      $(this).pointer('open');
-    }, function () {
-      var el = $(this);
-      setTimeout(function () {
-        if (!el.hasClass('open') && $('.ac-wp-pointer.hover').length == 0) {
-          el.pointer('close');
-        }
-      }, 100);
-    }).on('close', function () {
-      if (!el.hasClass('open') && $('.ac-wp-pointer.hover').length == 0) {
-        el.pointer('close');
-      }
-    });
+    ac_pointer($(this));
   });
   $('.ac-wp-pointer').hover(function () {
     $(this).addClass('hover');
   }, function () {
     $(this).removeClass('hover');
     $('.ac-pointer').trigger('close');
+  });
+}
+
+function ac_pointer($el) {
+  var el = $el,
+      $ = jQuery,
+      html = el.attr('rel'),
+      pos = el.attr('data-pos'),
+      w = el.attr('data-width'),
+      noclick = el.attr('data-noclick');
+  var position = {
+    at: 'left top',
+    // position of wp-pointer relative to the element which triggers the pointer event
+    my: 'right top',
+    // position of wp-pointer relative to the at-coordinates
+    edge: 'right' // position of arrow
+
+  };
+  var width = w ? w : 250;
+
+  if ('right' === pos) {
+    position = {
+      at: 'right middle',
+      my: 'left middle',
+      edge: 'left'
+    };
+  }
+
+  if ('left' === pos) {
+    position = {
+      at: 'left middle',
+      my: 'right middle',
+      edge: 'right'
+    };
+  } // create pointer
+
+
+  el.pointer({
+    content: $('#' + html).html(),
+    position: position,
+    pointerWidth: width,
+    // bug fix. with an arrow on the right side the position of wp-pointer is incorrect. it does not take
+    // into account the padding of the arrow. adding "wp-pointer-' + position.edge"  will fix that.
+    pointerClass: 'ac-wp-pointer wp-pointer wp-pointer-' + position.edge + (noclick ? ' noclick' : '')
+  }); // click
+
+  if (!noclick) {
+    el.click(function () {
+      if (el.hasClass('open')) {
+        el.removeClass('open');
+      } else {
+        el.addClass('open');
+      }
+    });
+  } // show on hover
+
+
+  el.hover(function () {
+    $(this).pointer('open');
+  }, function () {
+    var el = $(this);
+    setTimeout(function () {
+      if (!el.hasClass('open') && $('.ac-wp-pointer.hover').length == 0) {
+        el.pointer('close');
+      }
+    }, 100);
+  }).on('close', function () {
+    if (!el.hasClass('open') && $('.ac-wp-pointer.hover').length == 0) {
+      el.pointer('close');
+    }
   });
 }
 /*
