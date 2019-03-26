787fed225612d7f559a17b25f57bf2220d9898d8
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2019-03-26 09:48:12 +0100

    More Languages

--- codepress-admin-columns.php
+++ codepress-admin-columns.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Admin Columns
-Version: 3.4.1
+Version: 3.4.2
 Description: Customize columns on the administration screens for post(types), pages, media, comments, links and users with an easy to use drag-and-drop interface.
 Author: AdminColumns.com
 Author URI: https://www.admincolumns.com
@@ -36,7 +36,7 @@ if ( ! is_admin() ) {
 }
 
 define( 'AC_FILE', __FILE__ );
-define( 'AC_VERSION', '3.4.1' );
+define( 'AC_VERSION', '3.4.2' );
 
 require_once __DIR__ . '/classes/Dependencies.php';
 
@@ -53,10 +53,10 @@ add_action( 'after_setup_theme', function () {
 
 	AC\Autoloader::instance()->register_prefix( 'AC', __DIR__ . '/classes' );
 	AC\Autoloader\Underscore::instance()
-		->add_alias( 'AC\ListScreen', 'AC_ListScreen' )
-		->add_alias( 'AC\Settings\FormatValue', 'AC_Settings_FormatValueInterface' )
-		->add_alias( 'AC\Column\Media\MediaParent', 'AC_Column_Media_Parent' )
-		->add_alias( 'AC\Column\Post\PostParent', 'AC_Column_Post_Parent' );
+	                        ->add_alias( 'AC\ListScreen', 'AC_ListScreen' )
+	                        ->add_alias( 'AC\Settings\FormatValue', 'AC_Settings_FormatValueInterface' )
+	                        ->add_alias( 'AC\Column\Media\MediaParent', 'AC_Column_Media_Parent' )
+	                        ->add_alias( 'AC\Column\Post\PostParent', 'AC_Column_Post_Parent' );
 
 	/**
 	 * For loading external resources, e.g. column settings.
