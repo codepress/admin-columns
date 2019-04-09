9af507ad7aa7db64c1d610451d83eb2ef8058764
David Mosterd <david@codepress.nl>
2019-04-09 13:14:57 +0200

    Capabilities

---
+++ classes/Capabilities.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AC;
+
+use WP_Roles;
+use WP_User;
+
+abstract class Capabilities implements Registrable {
+
+	/**
+	 * @var WP_User
+	 */
+	protected $user;
+
+	public function __construct( WP_User $user = null ) {
+		if ( null === $user ) {
+			$user = wp_get_current_user();
+		}
+
+		$this->user = $user;
+	}
+
+	public function register() {
+		add_action( 'ac/capabilities/set_defaults', array( $this, 'set_default_caps' ) );
+	}
+
+	/**
+	 * @param string $cap
+	 *
+	 * @return bool
+	 */
+	public function has_cap( $cap ) {
+		return $this->user->has_cap( $cap );
+	}
+
+	abstract public function set_default_caps( WP_Roles $roles );
+
+}
