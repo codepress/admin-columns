decd0d7ed0401fb06ed0e8c1316fd590dd8a9a24
Tobias Schutter <tschutter@gmail.com>
2019-09-18 14:21:14 +0200

    naming

--- classes/InitializeColumns.php
+++ classes/InitializeColumns.php
@@ -19,7 +19,7 @@ class InitializeColumns implements Registrable {
 	public function scripts() {
 		$urls = array();
 
-		foreach ( $this->get_unitialized_list_screens() as $list_screen ) {
+		foreach ( $this->get_uninitialized_list_screens() as $list_screen ) {
 			$urls[] = array(
 				'screen_link' => add_query_arg( array( 'acp_action' => 'store_default_columns' ), $list_screen->get_screen_link() ),
 				'label'       => $list_screen->get_label()
@@ -29,7 +29,7 @@ echo '<pre>'; print_r( $urls ); echo '</pre>'; //exit;
 
 	}
 
-	private function get_unitialized_list_screens() {
+	private function get_uninitialized_list_screens() {
 		$list_screens = AC()->get_list_screens();
 
 		foreach ( $list_screens as $key => $list_screen ) {
