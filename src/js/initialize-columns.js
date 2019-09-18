ddb86f8d6a5d893ea2c0794a3a7238fe2ed1997d
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2019-09-18 15:18:51 +0200

    Test

--- src/js/initialize-columns.js
+++ src/js/initialize-columns.js
@@ -37,37 +37,6 @@ class InitializeColumns {
 	}
 }
 
-class Modal {
-	getMarkup(){
-		`<div class="ac-modal -active">
-			<div class="ac-modal__dialog">
-				<div class="ac-modal__dialog__content">
-					<p class="ac-modal__dialog__content__lead">
-						Upgrade to PRO, and take Admin Columns to the next level:			</p>
-					<ul class="ac-modal__dialog__list">
-						<li>Sort &amp; Filter on all your content</li>
-						<li>Directly edit your content from the overview</li>
-						<li>Export all column data to CSV</li>
-						<li>Create multiple column groups per overview</li>
-						<li>Get add-ons for ACF, WooCommerce and many more</li>
-					</ul>
-				</div>
-			
-			</div>
-		</div>`;
-	}
-
-	place(){
-		document.createElement()
-	}
-
-	remove(){
-
-	}
-}
-
-
-
 jQuery( document ).ready( function() {
 
 
