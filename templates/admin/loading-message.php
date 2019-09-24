b3a4c08b6c76038df085eb9bdcaf8d84839fe8e1
Tobias Schutter <tschutter@gmail.com>
2019-09-24 10:54:17 +0200

    wip

--- templates/admin/loading-message.php
+++ templates/admin/loading-message.php
@@ -4,14 +4,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-/**
- * @global string $price
- */
-
 ?>
 <div class="ac-loading-msg-wrapper">
 	<div class="ac-loading-msg">
 		<img class="ac-loading-msg__logo" src="<?php echo AC()->get_url() . '/assets/images/logo-ac.svg'; ?>" alt="Admin Columns">
-		<div class="ac-loading-msg__content">Loading Columns</div>
+		<div class="ac-loading-msg__content"><?php _e( 'Loading Columns', 'codepress-admin-columns' ); ?></div>
 	</div>
 </div>
