cbe8ffd2c7f2d039398b354c68e39fa1eaf20783
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2019-11-19 11:28:08 +0100

    Changelog Version Bump

--- codepress-admin-columns.php
+++ codepress-admin-columns.php
@@ -1,12 +1,12 @@
 <?php
 /*
 Plugin Name: Admin Columns
-Version: 3.4.7
+Version: 3.4.8
 Description: Customize columns on the administration screens for post(types), pages, media, comments, links and users with an easy to use drag-and-drop interface.
 Author: AdminColumns.com
 Author URI: https://www.admincolumns.com
 Plugin URI: https://www.admincolumns.com
-Requires PHP: 5.3.6
+Requires PHP: 5.6.20
 Text Domain: codepress-admin-columns
 Domain Path: /languages
 License: GPL v3
@@ -36,7 +36,7 @@ if ( ! is_admin() ) {
 }
 
 define( 'AC_FILE', __FILE__ );
-define( 'AC_VERSION', '3.4.7' );
+define( 'AC_VERSION', '3.4.8' );
 
 require_once __DIR__ . '/classes/Dependencies.php';
 
