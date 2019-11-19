c02a34899c6248d744aef5d01bedbcabb5b84490
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2019-11-19 10:44:17 +0100

    WIP

--- classes/Check/Promotion.php
+++ classes/Check/Promotion.php
@@ -3,7 +3,11 @@
 namespace AC\Check;
 
 use AC\Admin\Promo;
+use AC\Ajax;
+use AC\Message\Notice\Dismissible;
+use AC\Preferences;
 use AC\Registrable;
+use AC\Screen;
 
 final class Promotion
 	implements Registrable {
@@ -17,6 +21,40 @@ final class Promotion
 
 	public function register() {
 		// todo: see AddonAvailable
+
 	}
 
+	/**
+	 * @return Ajax\Handler
+	 */
+	private function get_ajax_handler() {
+		$handler = new Ajax\Handler();
+		$handler
+			->set_action( 'ac_dismiss_notice_addon_' . $this->get_individual_slug() )
+			->set_callback( array( $this, 'ajax_dismiss_notice' ) );
+
+		return $handler;
+	}
+
+	private function get_individual_slug() {
+		return $this->promo->get_slug() . $this->promo->get_date_range()->get_start()->format( 'Ymd' );
+	}
+
+	/**
+	 * @return Preferences\User
+	 */
+	private function get_preferences() {
+		return new Preferences\User( 'check-addon-available-' . $this->get_individual_slug() );
+	}
+
+	/**
+	 * @param Screen $screen
+	 */
+	public function display( Screen $screen ) {
+
+		$message = $this->promo->get_title();
+
+		$notice = new Dismissible( $message, $this->get_ajax_handler() );
+		$notice->register();
+	}
 }
