71f1591f8f8ebfcf09459c8a2ad4802c478991c9
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2019-11-26 15:59:27 +0100

    Styling and Table

---
+++ classes/Admin/Table.php
@@ -0,0 +1,36 @@
+<?php
+namespace AC\Admin;
+
+use AC\View;
+
+abstract class Table {
+
+	abstract public function getColumns();
+
+	abstract public function getItems();
+
+	abstract public function renderColumn( $name, $item );
+
+	public function getRowClass( $item ) {
+		return '';
+	}
+
+	public function getTableClasses() {
+		return [ 'widefat', 'fixed', 'ac-table' ];
+	}
+
+	public function render() {
+		$view = new View( [
+			'table' => $this,
+		] );
+
+		$view->set_template( 'admin/table' );
+
+		echo $view->render();
+	}
+
+	public function getColumnNames() {
+		return array_keys( $this->getColumns() );
+	}
+
+}
