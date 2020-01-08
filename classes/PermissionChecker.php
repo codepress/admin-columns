9bf769349d768f6960206a14a53fb17a4eb8c468
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2020-01-08 15:06:03 +0100

    Check user permissions

--- classes/PermissionChecker.php
+++ classes/PermissionChecker.php
@@ -21,6 +21,7 @@ class PermissionChecker {
 	public function is_valid( ListScreen $list_screen ) {
 		$roles = $list_screen->get_preference( 'roles' );
 		$users = $list_screen->get_preference( 'users' );
+		$users = is_array( $users ) ? array_map( 'intval', $users ) : [];
 
 		if ( empty( $users ) && empty( $roles ) ) {
 			return $list_screen;
@@ -34,9 +35,8 @@ class PermissionChecker {
 			}
 		}
 
-		if ( $users && is_array( $users ) && in_array( $this->user->ID, $users, true ) ) {
+		if ( $users && in_array( $this->user->ID, $users, true ) ) {
 			return $list_screen;
-
 		}
 
 		return null;
