e188c667f9de5c35ed7c53ffe276afc9133e245c
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2020-02-07 16:22:03 +0100

    WIP

--- templates/settings/section.php
+++ templates/settings/section.php
@@ -19,6 +19,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 					</div>
 				<?php endif; ?>
 
+				<?php if( $this->instructions  ): ?>
+
+					<a class="ac-pointer instructions" rel="pointer-export-5e39458051777" data-pos="right" data-pos_edge="" data-width="300">
+						<img src="https://acp.test/wp-content/plugins/admin-columns-pro/admin-columns/assets/images/question.svg" alt="?" class="ac-setting-input__info">
+					</a>
+					<div id="pointer-smart-filtering-5e39458051777" style="display:none;">
+						<?= $this->instructions; ?>
+					</div>
+				<?php endif; ?>
+
 				<?php if ( $this->read_more ) : ?>
 					<a href="<?php echo esc_url( $this->read_more ); ?>" target="_blank" class="more-link">
 						<span class="dashicons dashicons-admin-links"></span>
