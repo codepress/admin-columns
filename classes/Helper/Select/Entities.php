e1645f0380e0274d5aa7bb441b8ad2da0f9553cf
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2020-03-24 16:35:49 +0100

    Filter empty from entities
    This will give an error in the Select2 library

--- classes/Helper/Select/Entities.php
+++ classes/Helper/Select/Entities.php
@@ -12,6 +12,7 @@ class Entities extends ArrayIterator {
 	 */
 	public function __construct( array $entities, Value $value ) {
 		$value_entity_map = [];
+		$entities = array_filter( $entities );
 
 		foreach ( $entities as $entity ) {
 			$value_entity_map[ $value->get_value( $entity ) ] = $entity;
