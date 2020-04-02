166dcc8d74c8fad7c00bf84d68a41190f2797391
Tobias Schutter <tschutter@gmail.com>
2020-04-02 11:34:06 +0200

    Use native filters for value retrieval

--- classes/ListTable/Taxonomy.php
+++ classes/ListTable/Taxonomy.php
@@ -3,27 +3,20 @@
 namespace AC\ListTable;
 
 use AC\ListTable;
-use WP_Terms_List_Table;
 
 class Taxonomy implements ListTable {
-
-	/**
-	 * @var WP_Terms_List_Table
-	 */
-	private $table;
 
 	/**
 	 * @var string
 	 */
 	private $taxonomy;
 
-	public function __construct( WP_Terms_List_Table $table, $taxonomy ) {
-		$this->table = $table;
+	public function __construct( $taxonomy ) {
 		$this->taxonomy = $taxonomy;
 	}
 
 	public function get_column_value( $column, $id ) {
-		return $this->table->column_default( get_term_by( 'id', $id, $this->taxonomy ), $column );
+		return apply_filters( "manage_{$this->taxonomy}_custom_column", '', $column, $id );
 	}
 
 }
