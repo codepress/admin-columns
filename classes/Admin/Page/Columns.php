aaaeb57cc7cdd45b8c25584ffe0389de7e162b0b
Tobias Schutter <tschutter@gmail.com>
2020-04-08 11:20:27 +0200

    ListScreen Label

--- classes/Admin/Page/Columns.php
+++ classes/Admin/Page/Columns.php
@@ -60,7 +60,7 @@ class Columns extends Page implements Enqueueables, Helpable {
 
 	public function show_read_only_notice( ListScreen $list_screen ) {
 		if ( $list_screen->is_read_only() ) {
-			$message = sprintf( __( 'The columns for %s are read only and can therefore not be edited.', 'codepress-admin-columns' ), '<strong>' . esc_html( $list_screen->get_label() ) . '</strong>' );
+			$message = sprintf( __( 'The columns for %s are read only and can therefore not be edited.', 'codepress-admin-columns' ), '<strong>' . esc_html( $list_screen->get_title() ? $list_screen->get_title() : $list_screen->get_label() ) . '</strong>' );
 			$message = sprintf( '<p>%s</p>', apply_filters( 'ac/read_only_message', $message, $list_screen ) );
 
 			$notice = new Message\InlineMessage( $message );
