326f0336c185de6002f5cfae4a42f693daa794a2
Tobias Schutter <tschutter@gmail.com>
2020-05-06 19:05:28 +0200

    Full name

--- classes/Settings/Column/User.php
+++ classes/Settings/Column/User.php
@@ -9,7 +9,7 @@ class User extends Settings\Column implements Settings\FormatValue {
 
 	const PROPERTY_DISPLAY_NAME = 'display_name';
 	const PROPERTY_EMAIL = 'user_email';
-	const PROPERTY_FIRST_AND_LAST_NAME = 'first_last_name';
+	const PROPERTY_FULL_NAME = 'first_last_name';
 	const PROPERTY_FIRST_NAME = 'first_name';
 	const PROPERTY_ID = 'ID';
 	const PROPERTY_LAST_NAME = 'last_name';
@@ -71,16 +71,16 @@ class User extends Settings\Column implements Settings\FormatValue {
 	 */
 	protected function get_display_options() {
 		$options = [
-			self::PROPERTY_DISPLAY_NAME        => __( 'Display Name', 'codepress-admin-columns' ),
-			self::PROPERTY_FIRST_NAME          => __( 'First Name', 'codepress-admin-columns' ),
-			self::PROPERTY_FIRST_AND_LAST_NAME => __( 'First and Last Name', 'codepress-admin-columns' ),
-			self::PROPERTY_LAST_NAME           => __( 'Last Name', 'codepress-admin-columns' ),
-			self::PROPERTY_NICKNAME            => __( 'Nickname', 'codepress-admin-columns' ),
-			self::PROPERTY_ROLES               => __( 'Roles', 'codepress-admin-columns' ),
-			self::PROPERTY_LOGIN               => __( 'User Login', 'codepress-admin-columns' ),
-			self::PROPERTY_EMAIL               => __( 'User Email', 'codepress-admin-columns' ),
-			self::PROPERTY_ID                  => __( 'User ID', 'codepress-admin-columns' ),
-			self::PROPERTY_NICENAME            => __( 'User Nicename', 'codepress-admin-columns' ),
+			self::PROPERTY_DISPLAY_NAME => __( 'Display Name', 'codepress-admin-columns' ),
+			self::PROPERTY_FIRST_NAME   => __( 'First Name', 'codepress-admin-columns' ),
+			self::PROPERTY_FULL_NAME    => __( 'Full Name', 'codepress-admin-columns' ),
+			self::PROPERTY_LAST_NAME    => __( 'Last Name', 'codepress-admin-columns' ),
+			self::PROPERTY_NICKNAME     => __( 'Nickname', 'codepress-admin-columns' ),
+			self::PROPERTY_ROLES        => __( 'Roles', 'codepress-admin-columns' ),
+			self::PROPERTY_LOGIN        => __( 'User Login', 'codepress-admin-columns' ),
+			self::PROPERTY_EMAIL        => __( 'User Email', 'codepress-admin-columns' ),
+			self::PROPERTY_ID           => __( 'User ID', 'codepress-admin-columns' ),
+			self::PROPERTY_NICENAME     => __( 'User Nicename', 'codepress-admin-columns' ),
 		];
 
 		// resort for possible translations
