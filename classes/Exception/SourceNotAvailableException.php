e950bf89ff7f01eff53f8e03231983de869b00d2
Tobias Schutter <tschutter@gmail.com>
2020-05-19 19:34:21 +0200

    PHP7 compat

--- classes/Exception/SourceNotAvailableException.php
+++ classes/Exception/SourceNotAvailableException.php
@@ -3,12 +3,11 @@
 namespace AC\Exception;
 
 use RuntimeException;
-use Throwable;
 
 class SourceNotAvailableException extends RuntimeException {
 
-	public function __construct( $code = 0, Throwable $previous = null ) {
-		parent::__construct( 'No source available.', $code, $previous );
+	public function __construct( $code = 0 ) {
+		parent::__construct( 'No source available.', $code );
 	}
 
 }
