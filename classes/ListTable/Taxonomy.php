8267f4a927fb65c198edd9a0050f1bf60edc916c
David Mosterd <david@codepress.nl>
2021-03-16 11:49:19 -0400

    Typos

--- classes/ListTable/Taxonomy.php
+++ classes/ListTable/Taxonomy.php
@@ -15,15 +15,11 @@ class Taxonomy implements ListTable {
 	private $taxonomy;
 
 	public function __construct( WP_Terms_List_Table $table, $taxonomy ) {
-
-		$table->get_ta
-
 		$this->table = $table;
 		$this->taxonomy = $taxonomy;
 	}
 
 	public function get_column_value( $column, $id ) {
-		$this->table->get_ta
 		return apply_filters( "manage_{$this->taxonomy}_custom_column", '', $column, $id );
 	}
 
