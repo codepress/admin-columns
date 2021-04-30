c10b36c8381c71674fecf3e811948460d508d316
Stefan van den Dungen Gronovius <stefan@codepress.nl>
2021-04-30 14:47:40 +0200

    NPM and package cleaning

--- src/package-scripts.js
+++ src/package-scripts.js
@@ -1,27 +1,24 @@
-let core_path = '../assets/';
+const npsUtils = require( "nps-utils" );
+const core_path = '../assets/';
 
 module.exports = {
 	scripts : {
 		build : {
-			production : 'nps "styles.production" & nps "scripts.production"',
-			development : 'nps "styles.development" & nps "scripts.development"',
+			default : npsUtils.concurrent.nps( 'styles', 'scripts', 'languages' ),
+			development : npsUtils.concurrent.nps( 'styles.development', 'scripts.development' ),
 		},
 		styles : {
+			default : npsUtils.concurrent.nps( 'styles.production', 'styles.clean' ),
 			production : `rimraf ${core_path}css/*.map && sass scss:${core_path}css/ --style=compressed --no-source-map`,
-			development : `sass scss:${core_path}css/ -w`
+			development : `sass scss:${core_path}css/ -w`,
+			clean : npsUtils.rimraf( `${core_path}css/*.map` )
 		},
 		scripts : {
-			production : `webpack --mode=production`,
+			default : `webpack --mode=production`,
 			development : `webpack --mode=development`,
 		},
-		webfont : {
-			build : `nps "webfont.copy_fonts" & nps "webfont.copy_scss" & nps "styles.production"`,
-			create_fonts : `webfont svg/*.svg --dest webfont/fonts --dest-styles webfont/scss`,
-			copy_fonts : `cp -a webfont/fonts/. ${core_path}fonts/`,
-			copy_scss : `cp webfont/scss/template.scss scss/_webfont.scss`,
-		},
 		languages : {
-			production : 'nps "languages.build_pot" & nps "languages.pull_language"',
+			default : 'nps "languages.build_pot" & nps "languages.pull_language"',
 			build_pot : "node languages.js",
 			pull_languages : "( cd .. ; ./getlangs )"
 		}
