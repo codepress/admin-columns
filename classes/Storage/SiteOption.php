b825fc4d1d44a78d3dc42bd8c7bd5479f17026f5
David Mosterd <david@codepress.nl>
2021-08-23 22:04:46 +0200

    Update SiteOption.php
    Both using the same interface, but the const was not DRY

--- classes/Storage/SiteOption.php
+++ classes/Storage/SiteOption.php
@@ -2,21 +2,7 @@
 
 namespace AC\Storage;
 
-class SiteOption implements KeyValuePair {
-
-	const OPTION_DEFAULT = 'default';
-
-	/**
-	 * @var string
-	 */
-	protected $key;
-
-	/**
-	 * @param string $key
-	 */
-	public function __construct( $key ) {
-		$this->key = $key;
-	}
+class SiteOption extends Option {
 
 	/**
 	 * @param array $args
