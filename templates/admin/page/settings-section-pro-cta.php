4cb269fdc356b967bf5568f424eb37fead90467d
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2021-08-31 16:33:58 +0200

    Tooltip pro features WIP

--- templates/admin/page/settings-section-pro-cta.php
+++ templates/admin/page/settings-section-pro-cta.php
@@ -2,24 +2,7 @@
 
 use AC\Type\Url\Site;
 use AC\Type\Url\UtmTags;
-
-// TODO
-$features = [
-	__( 'Sortable Columns', 'codepress-admin-columns' ),
-	__( 'Smart Filters', 'codepress-admin-columns' ),
-	__( 'Sticky Headers', 'codepress-admin-columns' ),
-	__( 'Multiple Table Views', 'codepress-admin-columns' ),
-	__( 'Unlimited Columns', 'codepress-admin-columns' ),
-	__( 'Inline Editing', 'codepress-admin-columns' ),
-	__( 'Bulk Editing', 'codepress-admin-columns' ),
-	__( 'Horizontal Scrolling', 'codepress-admin-columns' ),
-	__( 'Export Table to CSV', 'codepress-admin-columns' ),
-	__( 'Multisite Support', 'codepress-admin-columns' ),
-	__( 'Customize List Table', 'codepress-admin-columns' ),
-	__( 'Taxonomy support', 'codepress-admin-columns' ),
-	__( 'All integration add-ons', 'codepress-admin-columns' ),
-	__( 'Quick Add', 'codepress-admin-columns' ),
-];
+use AC\View;
 
 $url = ( new UtmTags( new Site( Site::PAGE_ABOUT_PRO ), 'upgrade' ) )->get_url();
 
@@ -30,9 +13,108 @@ $url = ( new UtmTags( new Site( Site::PAGE_ABOUT_PRO ), 'upgrade' ) )->get_url()
 	<p class="description"><?= $this->description; ?></p>
 
 	<ul class="ac-pro-list">
-		<?php foreach ( $features as $feature ) : // TODO tooltip ?>
-			<li class="ac-pro-list__item -feature"><a><?= $feature; ?></a></li>
-		<?php endforeach; ?>
+		<?=
+		( new View( [
+			'feature' => __( 'Sortable Columns', 'codepress-admin-columns' ),
+			'tooltip' => __( 'Any column can be turned into an ordered list.', 'codepress-admin-columns' ) . ' ' . __( 'Sort on numbers, names, prices or anything else.', 'codepress-admin-columns' ),
+		] ) )->set_template( 'admin/page/component/pro-feature-list-item' )->render();
+		?>
+
+		<?=
+		( new View( [
+			'feature' => __( 'Smart Filters', 'codepress-admin-columns' ),
+			'tooltip' => sprintf(
+				'%s %s',
+				__( 'Only show the content that matches the rules you set.', 'codepress-admin-columns' ),
+				__( 'Filters help you to get valuable insights about orders, users, posts, anything really.', 'codepress-admin-columns' )
+			),
+		] ) )->set_template( 'admin/page/component/pro-feature-list-item' )->render();
+		?>
+
+		<?=
+		( new View( [
+			'feature' => __( 'Sticky Headers', 'codepress-admin-columns' ),
+			'tooltip' => __( '', 'codepress-admin-columns' ),
+		] ) )->set_template( 'admin/page/component/pro-feature-list-item' )->render();
+		?>
+
+		<?=
+		( new View( [
+			'feature' => __( 'Multiple Table Views', 'codepress-admin-columns' ),
+			'tooltip' => __( '', 'codepress-admin-columns' ),
+		] ) )->set_template( 'admin/page/component/pro-feature-list-item' )->render();
+		?>
+
+		<?=
+		( new View( [
+			'feature' => __( 'Unlimited Columns', 'codepress-admin-columns' ),
+			'tooltip' => __( '', 'codepress-admin-columns' ),
+		] ) )->set_template( 'admin/page/component/pro-feature-list-item' )->render();
+		?>
+
+		<?=
+		( new View( [
+			'feature' => __( 'Inline Editing', 'codepress-admin-columns' ),
+			'tooltip' => __( '', 'codepress-admin-columns' ),
+		] ) )->set_template( 'admin/page/component/pro-feature-list-item' )->render();
+		?>
+
+		<?=
+		( new View( [
+			'feature' => __( 'Bulk Editing', 'codepress-admin-columns' ),
+			'tooltip' => __( '', 'codepress-admin-columns' ),
+		] ) )->set_template( 'admin/page/component/pro-feature-list-item' )->render();
+		?>
+
+		<?=
+		( new View( [
+			'feature' => __( 'Horizontal Scrolling', 'codepress-admin-columns' ),
+			'tooltip' => __( '', 'codepress-admin-columns' ),
+		] ) )->set_template( 'admin/page/component/pro-feature-list-item' )->render();
+		?>
+
+		<?=
+		( new View( [
+			'feature' => __( 'Export Table to CSV', 'codepress-admin-columns' ),
+			'tooltip' => __( '', 'codepress-admin-columns' ),
+		] ) )->set_template( 'admin/page/component/pro-feature-list-item' )->render();
+		?>
+
+		<?=
+		( new View( [
+			'feature' => __( 'Multisite Support', 'codepress-admin-columns' ),
+			'tooltip' => __( '', 'codepress-admin-columns' ),
+		] ) )->set_template( 'admin/page/component/pro-feature-list-item' )->render();
+		?>
+
+		<?=
+		( new View( [
+			'feature' => __( 'Customize List Table', 'codepress-admin-columns' ),
+			'tooltip' => __( '', 'codepress-admin-columns' ),
+		] ) )->set_template( 'admin/page/component/pro-feature-list-item' )->render();
+		?>
+
+		<?=
+		( new View( [
+			'feature' => __( 'Taxonomy Support', 'codepress-admin-columns' ),
+			'tooltip' => __( '', 'codepress-admin-columns' ),
+		] ) )->set_template( 'admin/page/component/pro-feature-list-item' )->render();
+		?>
+
+		<?=
+		( new View( [
+			'feature' => __( 'All integration add-ons', 'codepress-admin-columns' ),
+			'tooltip' => __( '', 'codepress-admin-columns' ),
+		] ) )->set_template( 'admin/page/component/pro-feature-list-item' )->render();
+		?>
+
+		<?=
+		( new View( [
+			'feature' => __( 'Quick Add', 'codepress-admin-columns' ),
+			'tooltip' => __( '', 'codepress-admin-columns' ),
+		] ) )->set_template( 'admin/page/component/pro-feature-list-item' )->render();
+		?>
+
 		<li class="ac-pro-list__item">
 			<a target="_blank" href="<?= esc_url( $url ); ?>" class="ac-pro-list__item__link">
 				<?= __( 'View all Pro features', 'codepress-admin-columns' ); ?>
