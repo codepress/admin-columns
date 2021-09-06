622e73db7b3835746e253b82328ffc81cbf7baec
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2021-09-06 16:20:46 +0200

    Inline Script helper

--- classes/Admin/Asset/Settings.php
+++ classes/Admin/Asset/Settings.php
@@ -9,16 +9,9 @@ class Settings extends Script {
 	public function register() {
 		parent::register();
 
-		wp_add_inline_script(
-			$this->get_handle(),
-			sprintf(
-				"var AC = %s;",
-				json_encode( [
-					'_ajax_nonce' => wp_create_nonce( 'ac-ajax' ),
-				] )
-			),
-			'before'
-		);
+		$this->add_inline_variable( 'AC', [
+			'_ajax_nonce' => wp_create_nonce( 'ac-ajax' ),
+		] );
 	}
 
 }
