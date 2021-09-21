bbc18b3b1cfb402fb57c692df8bec96f7fafa330
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2021-09-21 10:45:29 +0200

    New views

---
+++ templates/admin/page/component/addon/deactivate.php
@@ -0,0 +1,3 @@
+<a href="<?php echo esc_url( $this->url ); ?>" class="ac-addon__link link-deactivate">
+	<?php _e( 'Deactivate', 'codepress-admin-columns' ); ?>
+</a>
---
+++ templates/admin/page/component/addon/missing-license.php
@@ -0,0 +1,4 @@
+<div class="ac-addon__state">
+	<span class="dashicons dashicons-info-outline orange"></span>
+	<span class="ac-addon__state__label"><?php _e( 'Activate license', 'codepress-admin-columns' ); ?></span>
+</div>
