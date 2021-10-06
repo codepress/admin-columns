704b868048cdc882a10baae6ff6d72d2b230f730
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2021-10-06 11:00:08 +0200

    Useful H1 on page

--- templates/admin/page/settings.php
+++ templates/admin/page/settings.php
@@ -4,9 +4,12 @@
  */
 ?>
 
-<?php do_action( 'ac/settings/general/notice' ); ?>
 
-<h1 class="screen-reader-text"><?= __('Settings', 'codepress-admin-columns'); ?></h1>
+<h1 class="screen-reader-text"><?= __( 'Settings', 'codepress-admin-columns' ); ?></h1>
+
+<div class="cpac-admin-notices">
+	<?php do_action( 'ac/settings/general/notice' ); ?>
+</div>
 
 <div class="ac-settings">
 	<?php foreach ( $this->sections as $section ) : ?>
