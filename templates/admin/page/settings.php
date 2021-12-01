3703029c683b9bb8a16e764693af3d706d1787b7
Tobias Schutter <tobias@codepress.nl>
2021-12-01 13:02:15 +0100

    Removed

--- templates/admin/page/settings.php
+++ templates/admin/page/settings.php
@@ -4,12 +4,7 @@
  */
 ?>
 
-
 <h1 class="screen-reader-text"><?= __( 'Settings', 'codepress-admin-columns' ); ?></h1>
-
-<div class="cpac-admin-notices">
-	<?php do_action( 'ac/settings/general/notice' ); ?>
-</div>
 
 <div class="ac-settings">
 	<?php foreach ( $this->sections as $section ) : ?>
