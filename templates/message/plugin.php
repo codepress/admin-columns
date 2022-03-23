aa6e88a17afcf4454f972c58cde69b7fe004769a
Tobias Schutter <tobias@codepress.nl>
2022-03-23 11:03:36 +0100

    Icons

--- templates/message/plugin.php
+++ templates/message/plugin.php
@@ -23,7 +23,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <tr class="plugin-update-tr <?= esc_attr( $this->status ) ?>" data-slug="<?= esc_attr( basename( $this->plugin_basename ) ) ?>" data-plugin="<?= esc_attr( $this->plugin_basename ) ?>">
 	<td colspan="100%" class="plugin-update colspanchange">
-		<div class="notice notice-alt inline <?= esc_attr( $this->class ) ?>">
+		<div class="update-message notice notice-alt inline <?= esc_attr( $this->class ) ?>">
 			<p><?= $this->message ?></p>
 		</div>
 	</td>
