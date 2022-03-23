3114cb6d058d045c1c870d07b0be7da01d32de9d
Tobias Schutter <tobias@codepress.nl>
2022-03-23 10:50:31 +0100

    Removed icon from Inline plugin message

--- templates/message/plugin.php
+++ templates/message/plugin.php
@@ -7,24 +7,24 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <style>
-	.plugins tr[data-plugin='<?php echo $this->plugin_basename; ?>'] th,
-	.plugins tr[data-plugin='<?php echo $this->plugin_basename; ?>'] td {
+	.plugins tr[data-plugin='<?= $this->plugin_basename ?>'] th,
+	.plugins tr[data-plugin='<?= $this->plugin_basename ?>'] td {
 		box-shadow: none;
 	}
 
 	<?php if ( $this->icon ) : ?>
 
-	.plugins tr[data-plugin='<?php echo $this->plugin_basename; ?>'] .update-message p:before {
-		content: "<?php echo $this->icon ?>";
+	.plugins tr[data-plugin='<?= $this->plugin_basename ?>'] .update-message p:before {
+		content: "<?= $this->icon ?>";
 	}
 
 	<?php endif; ?>
 </style>
 
-<tr class="plugin-update-tr <?php echo esc_attr( $this->status ); ?>" data-slug="<?php echo esc_attr( basename( $this->plugin_basename ) ); ?>" data-plugin="<?php echo esc_attr( $this->plugin_basename ); ?>">
+<tr class="plugin-update-tr <?= esc_attr( $this->status ) ?>" data-slug="<?= esc_attr( basename( $this->plugin_basename ) ) ?>" data-plugin="<?= esc_attr( $this->plugin_basename ) ?>">
 	<td colspan="100%" class="plugin-update colspanchange">
-		<div class="update-message notice notice-alt inline <?php echo esc_attr( $this->class ); ?>">
-			<p><?php echo $this->message; ?></p>
+		<div class="notice notice-alt inline <?= esc_attr( $this->class ) ?>">
+			<p><?= $this->message ?></p>
 		</div>
 	</td>
 </tr>
