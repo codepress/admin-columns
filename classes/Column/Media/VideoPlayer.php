1b2a9a8d66e6346080754762b7bad2ac617d46ef
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2022-04-11 16:13:12 +0200

    Title

--- classes/Column/Media/VideoPlayer.php
+++ classes/Column/Media/VideoPlayer.php
@@ -31,7 +31,7 @@ class VideoPlayer extends Column implements Column\DetailedValue {
 		}
 
 		return 'modal' === $this->get_display_type()
-			? sprintf( '<a data-modal-value="%s" data-modal-title="' . get_the_title( $id ) . '">%s</a>', $url, __( 'Play', 'codepress-admin-columns' ) )
+			? sprintf( '<a data-modal-value="%s" data-modal-title="%s">%s</a>', $url, get_the_title( $id ),__( 'Play', 'codepress-admin-columns' ) )
 			: $this->get_video_embed( $url );
 	}
 
