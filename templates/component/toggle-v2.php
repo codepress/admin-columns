a82d7adbd5eca215b5732c746add49142167b21a
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2022-05-02 13:30:34 +0200

    Better Toggle container attributes

--- templates/component/toggle-v2.php
+++ templates/component/toggle-v2.php
@@ -9,7 +9,7 @@ $value = $this->value ?: '1';
 
 ?>
 
-<div class="ac-toggle-v2">
+<div class="ac-toggle-v2<?= $this->container_class ? ' ' . $this->container_class : '' ?>" <?= $this->container_attributes; ?>>
 	<span class="ac-toggle-v2__toggle">
 		<input name="<?= $this->name; ?>" value="<?= esc_attr( $this->unchecked_value ); ?>" type="hidden">
 		<input class="ac-toggle-v2__toggle__input" <?= $this->attributes; ?> type="checkbox" value="<?= $value; ?>" <?php if ( $this->checked ): ?>checked="checked"<?php endif; ?>>
