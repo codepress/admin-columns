a4264793737318d75ef2dcf1d0c03f1ff5a478da
David Mosterd <david@codepress.nl>
2022-05-19 07:20:36 +0200

    template tooltip

---
+++ templates/tooltip/conditional-format.php
@@ -0,0 +1,4 @@
+<h3><?= __( 'Conditional Format', 'codepress-admin-columns' ) ?></h3>
+<p>
+	<?= _x( 'Conditional Format allows you to format a value based on conditions you set.', 'conditional format help', 'codepress-admin-columns' ); ?>
+</p>
