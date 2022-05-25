ed2bacd4ddf1cf4a44fc54e247289cfad6f57c66
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2022-05-25 13:28:03 +0200

    WIP Pointer

---
+++ src/js/components/PointerModal.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    export let content = '';
+    export let position = '';
+    let onElement = false;
+
+    export let closeHandler;
+
+    export const isOnElement = (): boolean => {
+        return onElement;
+    }
+
+    const checkLeave = () => {
+        onElement = false;
+        closeHandler();
+    }
+
+</script>
+
+<div class="ac-pointer-modal -{position} " on:mouseenter={()=> onElement = true} on:mouseleave={() => checkLeave()}>
+	<div class="ac-pointer-modal__arrow" data-pos={position}>
+	</div>
+	<div class="ac-pointer-modal__content">
+		{@html content}
+	</div>
+</div>
