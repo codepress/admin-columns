5fca35170ecf311a7e8cd92064d90597f8b3e58e
Tobias Schutter <tobias@codepress.nl>
2022-10-17 10:07:46 +0200

    Promo

--- classes/PromoCollection.php
+++ classes/PromoCollection.php
@@ -3,7 +3,6 @@
 namespace AC;
 
 use AC\Promo\BlackFriday;
-use AC\Promo\Halloween;
 use AC\Type\DateRange;
 use DateTime;
 
@@ -11,7 +10,6 @@ class PromoCollection extends ArrayIterator {
 
 	public function __construct() {
 		parent::__construct( [
-			new Halloween( new DateRange( new DateTime( '2022-10-28' ), new DateTime( '2022-11-01' ) ), 'Halloween22' ),
 			new BlackFriday( new DateRange( new DateTime( '2022-11-25' ), new DateTime( '2021-11-30' ) ), 'BlackFriday22' ),
 		] );
 	}
@@ -21,7 +19,9 @@ class PromoCollection extends ArrayIterator {
 	 * @return Promo|null
 	 */
 	public function find_active() {
-		/** @var Promo $promo */
+		/**
+		 * @var Promo $promo
+		 */
 		foreach ( $this->array as $promo ) {
 			if ( $promo->is_active() ) {
 				return $promo;
