0beb5fbc2b80a56f5aaa4f498505a9e4091ea0d6
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2022-11-14 14:56:03 +0100

    Update BaseTranslation.php

--- classes/Translation/BaseTranslation.php
+++ classes/Translation/BaseTranslation.php
@@ -9,9 +9,6 @@ class BaseTranslation implements Translation {
 	 */
 	private $data;
 
-	/**
-	 * @param string $data
-	 */
 	public function __construct( array $data ) {
 		$this->data = $data;
 	}
@@ -19,12 +16,7 @@ class BaseTranslation implements Translation {
 	public function get_translation(): array {
 		return $this->data;
 	}
-
-	/**
-	 * @param string $suffix
-	 *
-	 * @return self
-	 */
+	
 	public function with_translation( Translation $translation ) {
 		$data = $this->get_translation() + $translation->get_translation();
 
