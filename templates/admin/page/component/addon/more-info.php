d2288ec5b5ebb5fb9cb008724f706701d6f19ba5
Tobias Schutter <tobias@codepress.nl>
2022-11-18 09:20:24 +0100

    Status

--- templates/admin/page/component/addon/more-info.php
+++ templates/admin/page/component/addon/more-info.php
@@ -1,3 +1,3 @@
-<a target="_blank" href="<?= esc_url( $this->url ); ?>" class="ac-addon__button button-primary <?= $this->class; ?>">
+<a target="_blank" href="<?= esc_url( $this->url ) ?>" class="ac-addon__button button-primary <?= $this->class ?>">
 	<?php esc_html_e( 'Get this add-on', 'codepress-admin-columns' ); ?>
 </a>
