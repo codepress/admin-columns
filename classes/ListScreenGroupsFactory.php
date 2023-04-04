dc502318c0d6a5446c81283ff7c10e81b1235b38
Tobias Schutter <tobias@codepress.nl>
2023-04-04 12:58:00 +0200

    Update ListScreenGroupsFactory.php

--- classes/ListScreenGroupsFactory.php
+++ classes/ListScreenGroupsFactory.php
@@ -10,8 +10,8 @@ class ListScreenGroupsFactory {
 		$groups->add( 'post', __( 'Post Type', 'codepress-admin-columns' ), 5 );
 		$groups->add( 'user', __( 'Users' ) );
 		$groups->add( 'media', __( 'Media' ) );
-		$groups->add( 'comment', __( 'Comments' ) );
-		$groups->add( 'link', __( 'Links' ), 15 );
+		$groups->add( 'comment', __( 'Comments' ), 20 );
+		$groups->add( 'link', __( 'Links' ), 40 );
 
 		do_action( 'ac/list_screen_groups', $groups );
 
