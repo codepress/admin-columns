e738f312a8d56f3195e1993d696ab4d0edbac8a8
Tobias Schutter <tobias@codepress.nl>
2023-04-05 14:52:47 +0200

    Yoast SEO

--- classes/Column/Comment/Response.php
+++ classes/Column/Comment/Response.php
@@ -4,9 +4,6 @@ namespace AC\Column\Comment;
 
 use AC\Column;
 
-/**
- * @since 3.0
- */
 class Response extends Column {
 
 	public function __construct() {
@@ -16,23 +13,6 @@ class Response extends Column {
 
 	public function register_settings() {
 		$this->get_setting( 'width' )->set_default( 15 );
-	}
-
-	/**
-	 * Response Column should not be displayed when viewing "Comments On".
-	 * The list table does this by checking if $post_id is set globally. We mimic this functionality here.
-	 * @see WP_Comments_List_Table::get_columns() for the WP implementation
-	 */
-	public function is_valid() {
-		global $current_screen, $post_id;
-
-		// TODO fix
-//		if ( $current_screen && $this->get_list_screen()->is_current_screen( $current_screen ) && $post_id ) {
-//			return false;
-//		}
-
-		return true;
-
 	}
 
 }
