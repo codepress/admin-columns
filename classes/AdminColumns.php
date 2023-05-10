d9a1bc098c13306d151af7a12e7e93d7dac7face
Tobias Schutter <tobias@codepress.nl>
2023-05-10 11:38:00 +0200

    Update AdminColumns.php

--- classes/AdminColumns.php
+++ classes/AdminColumns.php
@@ -45,8 +45,78 @@ class AdminColumns extends Plugin {
 	protected function __construct() {
 		parent::__construct( AC_FILE, new Version( AC_VERSION ) );
 
-		$is_acp_active = defined( 'ACP_FILE' );
+		$container = $this->create_container();
 
+		$this->storage = $container->get( Storage::class );
+
+		ListScreenFactory::add( $container->get( ListScreenFactory\UserFactory::class ) );
+		ListScreenFactory::add( $container->get( ListScreenFactory\CommentFactory::class ) );
+		ListScreenFactory::add( $container->get( ListScreenFactory\PostFactory::class ) );
+		ListScreenFactory::add( $container->get( ListScreenFactory\MediaFactory::class ) );
+
+		$page_handler = new PageRequestHandler();
+		$page_handler->add( 'columns', $container->get( Admin\PageFactory\Columns::class ) )
+		             ->add( 'settings', $container->get( Admin\PageFactory\Settings::class ) )
+		             ->add( 'addons', $container->get( Admin\PageFactory\Addons::class ) )
+		             ->add( 'help', $container->get( Admin\PageFactory\Help::class ) );
+
+		PageRequestHandlers::add_handler( $page_handler );
+
+		$this->register_services( $container );
+	}
+
+	private function register_services( DI\Container $container ): void {
+		$services_fqn = [
+			PluginActionLinks::class,
+			Screen::class,
+			Admin\Admin::class,
+			Admin\Scripts::class,
+			Admin\Notice\ReadOnlyListScreen::class,
+			Ajax\NumberFormat::class,
+			ThirdParty\ACF::class,
+			ThirdParty\NinjaForms::class,
+			ThirdParty\MediaLibraryAssistant\MediaLibraryAssistant::class,
+			ThirdParty\WooCommerce::class,
+			ThirdParty\WPML::class,
+			Controller\DefaultColumns::class,
+			Screen\QuickEdit::class,
+			Capabilities\Manage::class,
+			Controller\AjaxColumnRequest::class,
+			Controller\AjaxGeneralOptions::class,
+			Controller\AjaxRequestCustomFieldKeys::class,
+			Controller\AjaxColumnModalValue::class,
+			Controller\AjaxColumnValue::class,
+			Controller\AjaxScreenOptions::class,
+			Controller\ListScreenRestoreColumns::class,
+			RestoreSettingsRequest::class,
+			Controller\TableListScreenSetter::class,
+			Service\IntegrationColumns::class,
+			Service\CommonAssets::class,
+			Service\Colors::class,
+		];
+
+		if ( ! defined( 'ACP_FILE' ) ) {
+			$services_fqn[] = Service\NoticeChecks::class;
+			$services_fqn[] = PluginActionUpgrade::class;
+			$services_fqn[] = Service\ColumnsMockup::class;
+		}
+
+		array_map( static function ( string $service ) use ( $container ): void {
+			$container->get( $service )->register();
+		}, $services_fqn );
+
+		$services[] = new Service\Setup( $container->get( SetupFactory\AdminColumns::class )->create( SetupFactory::SITE ) );
+
+		if ( $this->is_network_active() ) {
+			$services[] = new Service\Setup( $container->get( SetupFactory\AdminColumns::class )->create( SetupFactory::NETWORK ) );
+		}
+
+		array_map( static function ( Registerable $service ): void {
+			$service->register();
+		}, $services );
+	}
+
+	private function create_container(): DI\Container {
 		$definitions = [
 			'translations.global'                   => function (): Translation {
 				return new Translation( require $this->get_dir() . '/settings/translations/global.php' );
@@ -81,7 +151,7 @@ class AdminColumns extends Plugin {
 				->constructorParameter( 0, admin_url( 'options-general.php' ) ),
 			Admin\MenuListFactory::class            => DI\autowire( Admin\MenuListFactory\MenuFactory::class ),
 			Admin\PageFactory\Settings::class       => DI\autowire()
-				->constructorParameter( 2, $is_acp_active ),
+				->constructorParameter( 2, defined( 'ACP_FILE' ) ),
 			SetupFactory\AdminColumns::class        => DI\autowire()
 				->constructorParameter( 0, 'ac_version' )
 				->constructorParameter( 1, $this->get_version() ),
@@ -89,73 +159,9 @@ class AdminColumns extends Plugin {
 				->constructorParameter( 0, DI\get( SetupFactory\AdminColumns::class ) ),
 		];
 
-		$container = ( new ContainerBuilder() )
+		return ( new ContainerBuilder() )
 			->addDefinitions( $definitions )
 			->build();
-
-		$this->storage = $container->get( Storage::class );
-
-		ListScreenFactory::add( $container->get( ListScreenFactory\UserFactory::class ) );
-		ListScreenFactory::add( $container->get( ListScreenFactory\CommentFactory::class ) );
-		ListScreenFactory::add( $container->get( ListScreenFactory\PostFactory::class ) );
-		ListScreenFactory::add( $container->get( ListScreenFactory\MediaFactory::class ) );
-
-		$page_handler = new PageRequestHandler();
-		$page_handler->add( 'columns', $container->get( Admin\PageFactory\Columns::class ) )
-		             ->add( 'settings', $container->get( Admin\PageFactory\Settings::class ) )
-		             ->add( 'addons', $container->get( Admin\PageFactory\Addons::class ) )
-		             ->add( 'help', $container->get( Admin\PageFactory\Help::class ) );
-
-		PageRequestHandlers::add_handler( $page_handler );
-
-		$services_fqn = [
-			PluginActionLinks::class,
-			Screen::class,
-			Admin\Admin::class,
-			Admin\Scripts::class,
-			Admin\Notice\ReadOnlyListScreen::class,
-			Ajax\NumberFormat::class,
-			ThirdParty\ACF::class,
-			ThirdParty\NinjaForms::class,
-			ThirdParty\MediaLibraryAssistant\MediaLibraryAssistant::class,
-			ThirdParty\WooCommerce::class,
-			ThirdParty\WPML::class,
-			Controller\DefaultColumns::class,
-			Screen\QuickEdit::class,
-			Capabilities\Manage::class,
-			Controller\AjaxColumnRequest::class,
-			Controller\AjaxGeneralOptions::class,
-			Controller\AjaxRequestCustomFieldKeys::class,
-			Controller\AjaxColumnModalValue::class,
-			Controller\AjaxColumnValue::class,
-			Controller\AjaxScreenOptions::class,
-			Controller\ListScreenRestoreColumns::class,
-			RestoreSettingsRequest::class,
-			Controller\TableListScreenSetter::class,
-			Service\IntegrationColumns::class,
-			Service\CommonAssets::class,
-			Service\Colors::class,
-		];
-
-		if ( ! $is_acp_active ) {
-			$services_fqn[] = Service\NoticeChecks::class;
-			$services_fqn[] = PluginActionUpgrade::class;
-			$services_fqn[] = Service\ColumnsMockup::class;
-		}
-
-		array_map( static function ( string $service ) use ( $container ): void {
-			$container->get( $service )->register();
-		}, $services_fqn );
-
-		$services[] = new Service\Setup( $container->get( SetupFactory\AdminColumns::class )->create( SetupFactory::SITE ) );
-
-		if ( $this->is_network_active() ) {
-			$services[] = new Service\Setup( $container->get( SetupFactory\AdminColumns::class )->create( SetupFactory::NETWORK ) );
-		}
-
-		array_map( static function ( Registerable $service ): void {
-			$service->register();
-		}, $services );
 	}
 
 	private function is_network_active(): bool {
