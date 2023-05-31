ef1df38c66015b2a2ea05c1f85d2d011cdeb8efa
David Mosterd <david@codepress.nl>
2023-05-31 15:51:50 +0200

    initial basefactory stub

--- classes/ListScreenFactory/PostFactory.php
+++ classes/ListScreenFactory/PostFactory.php
@@ -1,55 +1,50 @@
 <?php
-declare( strict_types=1 );
+
+declare(strict_types=1);
 
 namespace AC\ListScreenFactory;
 
 use AC\ListScreen;
 use AC\ListScreen\Post;
-use AC\ListScreenFactory;
 use AC\PostTypeRepository;
-use InvalidArgumentException;
 use WP_Screen;
 
-class PostFactory implements ListScreenFactory {
+class PostFactory extends BaseFactory
+{
 
-	use ListSettingsTrait;
+    private $post_type_repository;
 
-	private $post_type_repository;
+    public function __construct(PostTypeRepository $post_type_repository)
+    {
+        $this->post_type_repository = $post_type_repository;
+    }
 
-	public function __construct( PostTypeRepository $post_type_repository ) {
-		$this->post_type_repository = $post_type_repository;
-	}
+    protected function create_list_screen_from_wp_screen(WP_Screen $screen): ListScreen
+    {
+        return $this->create_list_screen($screen->post_type);
+    }
 
-	protected function create_list_screen( string $key ): Post {
-		return new Post( $key );
-	}
+    protected function create_list_screen(string $key): ListScreen
+    {
+        return new Post($key);
+    }
 
-	public function create( string $key, array $settings = [] ): ListScreen {
-		if ( ! $this->can_create( $key ) ) {
-			throw new InvalidArgumentException( 'Invalid key.' );
-		}
+    public function can_create(string $key): bool
+    {
+        return post_type_exists($key) && $this->is_supported_post_type($key);
+    }
 
-		return $this->add_settings( $this->create_list_screen( $key ), $settings );
-	}
+    private function is_supported_post_type(string $post_type): bool
+    {
+        return $this->post_type_repository->exists($post_type);
+    }
 
-	public function create_from_wp_screen( WP_Screen $screen, array $settings = [] ): ListScreen {
-		if ( ! $this->can_create_from_wp_screen( $screen ) ) {
-			throw new InvalidArgumentException( 'Invalid screen.' );
-		}
-
-		return $this->create( $screen->post_type, $settings );
-	}
-
-	public function can_create( string $key ): bool {
-		return post_type_exists( $key ) && $this->is_supported_post_type( $key );
-	}
-
-	private function is_supported_post_type( string $post_type ): bool {
-		return $this->post_type_repository->exists( $post_type );
-	}
-
-	public function can_create_from_wp_screen( WP_Screen $screen ): bool {
-		return 'edit' === $screen->base && $screen->post_type && 'edit-' . $screen->post_type === $screen->id && $this->is_supported_post_type( $screen->post_type );
-	}
+    public function can_create_from_wp_screen(WP_Screen $screen): bool
+    {
+        return 'edit' === $screen->base &&
+               $screen->post_type &&
+               'edit-' . $screen->post_type === $screen->id &&
+               $this->is_supported_post_type($screen->post_type);
+    }
 
 }
