2d7629346ba0b9620c4100dd0edbe61cc2e52335
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2023-06-02 10:14:04 +0200

    MLA ListScreenFactory

--- classes/ThirdParty/MediaLibraryAssistant/ListScreenFactory.php
+++ classes/ThirdParty/MediaLibraryAssistant/ListScreenFactory.php
@@ -4,44 +4,28 @@ declare( strict_types=1 );
 
 namespace AC\ThirdParty\MediaLibraryAssistant;
 
-use AC\ListScreen;
-use AC\ListScreenFactory\ListSettingsTrait;
 use AC;
+use AC\ListScreen;
 use AC\ThirdParty\MediaLibraryAssistant\ListScreen\MediaLibrary;
-use LogicException;
 use MLACore;
 use WP_Screen;
 
-class ListScreenFactory implements AC\ListScreenFactory {
+class ListScreenFactory extends AC\ListScreenFactory\BaseFactory {
 
-    use ListSettingsTrait;
+	public function can_create( string $key ): bool {
+		return 'mla-media-assistant' === $key;
+	}
 
-    public function can_create( string $key ): bool {
-        return 'mla-media-assistant' === $key;
-    }
+	public function can_create_from_wp_screen( WP_Screen $screen ): bool {
+		return class_exists( 'MLACore' ) && 'media_page_' . MLACore::ADMIN_PAGE_SLUG === $screen->id;
+	}
 
-    protected function create_list_screen(): MediaLibrary {
-        return new MediaLibrary();
-    }
+	protected function create_list_screen( string $key ): ListScreen {
+		return new MediaLibrary();
+	}
 
-    public function can_create_from_wp_screen( WP_Screen $screen ): bool {
-        return class_exists( 'MLACore' ) && 'media_page_' . MLACore::ADMIN_PAGE_SLUG === $screen->id;
-    }
-
-    public function create( string $key, array $settings = [] ): ListScreen {
-        if ( ! $this->can_create( $key ) ) {
-            throw new LogicException( 'Invalid screen' );
-        }
-
-        return $this->add_settings( $this->create_list_screen(), $settings );
-    }
-
-    public function create_from_wp_screen( WP_Screen $screen, array $settings = [] ): ListScreen {
-        if ( ! $this->can_create_from_wp_screen( $screen ) ) {
-            throw new LogicException( 'Invalid screen' );
-        }
-
-        return $this->add_settings( $this->create_list_screen(), $settings );
-    }
+	protected function create_list_screen_from_wp_screen( WP_Screen $screen ): ListScreen {
+		return $this->create_list_screen( 'mla-media-assistant' );
+	}
 
 }
