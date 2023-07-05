56d144b4570c9fbf5727f4dcc3c8a34a2d209b4f
Tobias Schutter <tobias@codepress.nl>
2023-07-05 14:49:44 +0200

    Update AdminColumns.php

--- classes/Plugin/SetupFactory/AdminColumns.php
+++ classes/Plugin/SetupFactory/AdminColumns.php
@@ -4,35 +4,37 @@ namespace AC\Plugin\SetupFactory;
 
 use AC\Plugin\Install;
 use AC\Plugin\InstallCollection;
+use AC\Plugin\Setup;
 use AC\Plugin\SetupFactory;
 use AC\Plugin\Update;
 use AC\Plugin\UpdateCollection;
 
-final class AdminColumns extends SetupFactory {
+final class AdminColumns extends SetupFactory
+{
 
-	public function create( $type ) {
+    public function create(string $type): Setup
+    {
+        switch ($type) {
+            case self::NETWORK:
+                $this->installers = new InstallCollection([
+                    new Install\Capabilities(),
+                ]);
+                break;
+            case self::SITE:
+                $this->installers = new InstallCollection([
+                    new Install\Capabilities(),
+                    new Install\Database(),
+                ]);
+                $this->updates = new UpdateCollection([
+                    new Update\V3005(),
+                    new Update\V3007(),
+                    new Update\V3201(),
+                    new Update\V4000(),
+                ]);
+                break;
+        }
 
-		switch ( $type ) {
-			case self::NETWORK:
-				$this->installers = new InstallCollection( [
-					new Install\Capabilities(),
-				] );
-				break;
-			case self::SITE:
-				$this->installers = new InstallCollection( [
-					new Install\Capabilities(),
-					new Install\Database(),
-				] );
-				$this->updates = new UpdateCollection( [
-					new Update\V3005(),
-					new Update\V3007(),
-					new Update\V3201(),
-					new Update\V4000(),
-				] );
-				break;
-		}
-
-		return parent::create( $type );
-	}
+        return parent::create($type);
+    }
 
 }
