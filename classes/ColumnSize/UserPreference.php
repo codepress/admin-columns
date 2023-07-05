051dac8ca386bfcc36415d16d660af0aaa8cf92a
Tobias Schutter <tobias@codepress.nl>
2023-07-05 10:43:06 +0200

    Formatting

--- classes/ColumnSize/UserPreference.php
+++ classes/ColumnSize/UserPreference.php
@@ -4,10 +4,12 @@ namespace AC\ColumnSize;
 
 use AC\Preferences\Site;
 
-class UserPreference extends Site {
+class UserPreference extends Site
+{
 
-	public function __construct( $user_id = null ) {
-		parent::__construct( 'column_widths', $user_id );
-	}
+    public function __construct(int $user_id = null)
+    {
+        parent::__construct('column_widths', $user_id);
+    }
 
 }
--- classes/Controller/TableListScreenSetter.php
+++ classes/Controller/TableListScreenSetter.php
@@ -12,68 +12,71 @@ use AC\Request;
 use AC\Table;
 use WP_Screen;
 
-class TableListScreenSetter implements Registerable {
+class TableListScreenSetter implements Registerable
+{
 
-	private $storage;
+    private $storage;
 
-	private $location;
+    private $location;
 
-	private $list_screen_factory;
+    private $list_screen_factory;
 
-	private $preference;
+    private $preference;
 
-	private $primary_column_factory;
+    private $primary_column_factory;
 
-	public function __construct(
-		Storage $storage,
-		Absolute $location,
-		ListScreenFactory $list_screen_factory,
-		Table\LayoutPreference $preference,
-		Table\PrimaryColumnFactory $primary_column_factory
-	) {
-		$this->storage = $storage;
-		$this->list_screen_factory = $list_screen_factory;
-		$this->location = $location;
-		$this->preference = $preference;
-		$this->primary_column_factory = $primary_column_factory;
-	}
+    public function __construct(
+        Storage $storage,
+        Absolute $location,
+        ListScreenFactory $list_screen_factory,
+        Table\LayoutPreference $preference,
+        Table\PrimaryColumnFactory $primary_column_factory
+    ) {
+        $this->storage = $storage;
+        $this->list_screen_factory = $list_screen_factory;
+        $this->location = $location;
+        $this->preference = $preference;
+        $this->primary_column_factory = $primary_column_factory;
+    }
 
-	public function register() {
-		add_action( 'current_screen', [ $this, 'handle' ] );
-	}
+    public function register()
+    {
+        add_action('current_screen', [$this, 'handle']);
+    }
 
-	public function handle( WP_Screen $wp_screen ): void {
-		$request = new Request();
+    public function handle(WP_Screen $wp_screen): void
+    {
+        $request = new Request();
 
-		$request->add_middleware(
-			new Middleware\ListScreenTable(
-				$this->storage,
-				$this->list_screen_factory,
-				$wp_screen,
-				$this->preference
-			)
-		);
+        $request->add_middleware(
+            new Middleware\ListScreenTable(
+                $this->storage,
+                $this->list_screen_factory,
+                $wp_screen,
+                $this->preference
+            )
+        );
 
-		$list_screen = $request->get( 'list_screen' );
+        $list_screen = $request->get('list_screen');
 
-		if ( ! $list_screen instanceof ListScreen ) {
-			return;
-		}
+        if ( ! $list_screen instanceof ListScreen) {
+            return;
+        }
 
-		if ( $list_screen->has_id() ) {
-			$this->preference->set( $list_screen->get_key(), (string) $list_screen->get_id() );
-		}
+        if ($list_screen->has_id()) {
+            $this->preference->set($list_screen->get_key(), (string)$list_screen->get_id());
+        }
 
-		$table_screen = new Table\Screen(
-			$this->location,
-			$list_screen,
-			new ColumnSize\ListStorage( $this->storage ),
-			new ColumnSize\UserStorage( new ColumnSize\UserPreference( get_current_user_id() ) ),
-			$this->primary_column_factory
-		);
-		$table_screen->register();
+        $table_screen = new Table\Screen(
+            $this->location,
+            $list_screen,
+            new ColumnSize\ListStorage($this->storage),
+            new ColumnSize\UserStorage(new ColumnSize\UserPreference()),
+            $this->primary_column_factory
+        );
+        $table_screen->register();
 
-		do_action( 'ac/table', $table_screen );
-	}
+        do_action('ac/table', $table_screen);
+    }
 
 }
