b9d973ddd676fb38d75c69f3c0b0756af867e3b6
Tobias Schutter <tobias@codepress.nl>
2023-07-05 14:09:07 +0200

    Formatting

--- classes/Admin/AdminScripts.php
+++ classes/Admin/AdminScripts.php
@@ -8,22 +8,25 @@ use AC\Asset\Location;
 use AC\Asset\Script;
 use AC\Asset\Style;
 
-class AdminScripts implements Enqueueables {
+class AdminScripts implements Enqueueables
+{
 
-	/**
-	 * @var Location\Absolute
-	 */
-	private $location;
+    /**
+     * @var Location\Absolute
+     */
+    private $location;
 
-	public function __construct( Location\Absolute $location ) {
-		$this->location = $location;
-	}
+    public function __construct(Location\Absolute $location)
+    {
+        $this->location = $location;
+    }
 
-	public function get_assets() {
-		return new Assets( [
-			new Script( 'ac-admin-general', $this->location->with_suffix( 'assets/js/admin-general.js' ), [ 'jquery' ] ),
-			new Style( 'ac-admin', $this->location->with_suffix( 'assets/css/admin-general.css' ), [ 'ac-ui' ] ),
-		] );
-	}
+    public function get_assets()
+    {
+        return new Assets([
+            new Script('ac-admin-general', $this->location->with_suffix('assets/js/admin-general.js'), ['jquery']),
+            new Style('ac-admin', $this->location->with_suffix('assets/css/admin-general.css'), ['ac-ui']),
+        ]);
+    }
 
 }
--- classes/Asset/Location/Absolute.php
+++ classes/Asset/Location/Absolute.php
@@ -4,51 +4,41 @@ namespace AC\Asset\Location;
 
 use AC\Asset\Location;
 
-final class Absolute implements Location {
+final class Absolute implements Location
+{
 
-	/**
-	 * @var string
-	 */
-	private $url;
+    /**
+     * @var string
+     */
+    private $url;
 
-	/**
-	 * @var string
-	 */
-	private $path;
+    /**
+     * @var string
+     */
+    private $path;
 
-	/**
-	 * @param string $url
-	 * @param string $path
-	 */
-	public function __construct( string $url, string $path ) {
-		$this->url = rtrim( $url, '/' );
-		$this->path = rtrim( $path, '/' );
-	}
+    public function __construct(string $url, string $path)
+    {
+        $this->url = rtrim($url, '/');
+        $this->path = rtrim($path, '/');
+    }
 
-	/**
-	 * @param string $suffix
-	 *
-	 * @return self
-	 */
-	public function with_suffix( $suffix ) {
-		$url = $this->get_url() . '/' . ltrim( $suffix, '/' );
-		$path = $this->get_path() . '/' . ltrim( $suffix, '/' );
+    public function with_suffix(string $suffix): self
+    {
+        $url = $this->get_url() . '/' . ltrim($suffix, '/');
+        $path = $this->get_path() . '/' . ltrim($suffix, '/');
 
-		return new self( $url, $path );
-	}
+        return new self($url, $path);
+    }
 
-	/**
-	 * @return string
-	 */
-	public function get_url() {
-		return $this->url;
-	}
+    public function get_url(): string
+    {
+        return $this->url;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function get_path() {
-		return $this->path;
-	}
+    public function get_path(): string
+    {
+        return $this->path;
+    }
 
 }
