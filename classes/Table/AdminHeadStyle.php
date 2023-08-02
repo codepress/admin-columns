ac4064592bd11cb2122e17d91419966fda107d53
Tobias Schutter <tobias@codepress.nl>
2023-08-02 16:43:05 +0200

    Update AdminHeadStyle.php

--- classes/Table/AdminHeadStyle.php
+++ classes/Table/AdminHeadStyle.php
@@ -17,14 +17,14 @@ class AdminHeadStyle implements Registerable
         add_action('admin_print_scripts', [$this, 'render']);
     }
 
-    public static function add(string $style_blocks): void
+    public static function add(string $style_block): void
     {
-        if ( ! ac_helper()->string->starts_with($style_blocks, '<style>') ||
-             ! ac_helper()->string->ends_with($style_blocks, '</style>')) {
+        if ( ! ac_helper()->string->starts_with($style_block, '<style>') ||
+             ! ac_helper()->string->ends_with($style_block, '</style>')) {
             throw new InvalidArgumentException('Block needs to be wrapped in "style" tags');
         }
 
-        self::$style_blocks[] = $style_blocks;
+        self::$style_blocks[] = $style_block;
     }
 
     public function render(): void
