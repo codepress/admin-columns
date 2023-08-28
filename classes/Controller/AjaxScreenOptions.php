a7624d31a650993360eeeed3ccca1774dc92b544
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2023-08-28 13:06:18 +0200

    Update AjaxScreenOptions.php

--- classes/Controller/AjaxScreenOptions.php
+++ classes/Controller/AjaxScreenOptions.php
@@ -6,41 +6,45 @@ use AC\Admin\Preference\ScreenOptions;
 use AC\Ajax;
 use AC\Registerable;
 
-class AjaxScreenOptions implements Registerable {
+class AjaxScreenOptions implements Registerable
+{
 
-	/**
-	 * @var ScreenOptions
-	 */
-	private $preference;
+    /**
+     * @var ScreenOptions
+     */
+    private $preference;
 
-	public function __construct( ScreenOptions $preference ) {
-		$this->preference = $preference;
-	}
-
-	public function register(): void
+    public function __construct(ScreenOptions $preference)
     {
-		$this->get_ajax_handler()->register();
-	}
+        $this->preference = $preference;
+    }
 
-	/**
-	 * @return Ajax\Handler
-	 */
-	private function get_ajax_handler() {
-		$handler = new Ajax\Handler();
-		$handler
-			->set_action( 'ac_admin_screen_options' )
-			->set_callback( [ $this, 'handle_ajax_request' ] );
+    public function register(): void
+    {
+        $this->get_ajax_handler()->register();
+    }
 
-		return $handler;
-	}
+    /**
+     * @return Ajax\Handler
+     */
+    private function get_ajax_handler()
+    {
+        $handler = new Ajax\Handler();
+        $handler
+            ->set_action('ac_admin_screen_options')
+            ->set_callback([$this, 'handle_ajax_request']);
 
-	public function handle_ajax_request() {
-		$this->get_ajax_handler()->verify_request();
+        return $handler;
+    }
 
-		$name = filter_input( INPUT_POST, 'option_name', FILTER_SANITIZE_FULL_SPECIAL_CHARS );
-		$value = (int) filter_input( INPUT_POST, 'option_value', FILTER_SANITIZE_NUMBER_INT );
+    public function handle_ajax_request()
+    {
+        $this->get_ajax_handler()->verify_request();
 
-		$this->preference->set( $name, $value );
-	}
+        $name = filter_input(INPUT_POST, 'option_name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $value = (int)filter_input(INPUT_POST, 'option_value', FILTER_SANITIZE_NUMBER_INT);
+
+        $this->preference->set($name, $value);
+    }
 
 }
