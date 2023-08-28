509def7cda8aed8dd8b2ae8e2245c1d8343da357
Tobias Schutter <tobias@codepress.nl>
2023-08-28 12:55:51 +0200

    Create ListScreenSource.php

---
+++ classes/Admin/ScreenOption/ListScreenSource.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AC\Admin\ScreenOption;
+
+use AC\Admin\Preference;
+use AC\Admin\ScreenOption;
+
+class ListScreenSource extends ScreenOption
+{
+
+    private const KEY = 'show_list_screen_source';
+
+    private $preference;
+
+    public function __construct(Preference\ScreenOptions $preference)
+    {
+        $this->preference = $preference;
+    }
+
+    public function is_active(): bool
+    {
+        return 1 === $this->preference->get(self::KEY);
+    }
+
+    public function render(): string
+    {
+        ob_start();
+        ?>
+
+		<label for="ac-list-screen-source" data-ac-screen-option="<?= self::KEY ?>">
+			<input id="ac-list-screen-source" type="checkbox" <?php
+            checked($this->is_active()); ?>>
+            <?= __('List Screen Source', 'codepress-admin-columns') ?>
+		</label>
+        <?php
+        return ob_get_clean();
+    }
+
+}
