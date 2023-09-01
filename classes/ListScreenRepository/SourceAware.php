f194ca21371032d1fc40f182a5681dad3b69ff75
Tobias Schutter <tobias@codepress.nl>
2023-09-01 14:22:22 +0200

    Update SourceAware.php

--- classes/ListScreenRepository/SourceAware.php
+++ classes/ListScreenRepository/SourceAware.php
@@ -3,18 +3,12 @@
 namespace AC\ListScreenRepository;
 
 use AC\Type\ListScreenId;
-use ACP\Storage\Directory;
-use SplFileInfo;
 
 interface SourceAware
 {
 
-    public function get_source_directory(): Directory;
+    public function get_source(ListScreenId $id): string;
 
-    public function has_source_directory(): bool;
-
-    public function get_source_file(ListScreenId $id): SplFileInfo;
-
-    public function has_source_file(ListScreenId $id): bool;
+    public function has_source(ListScreenId $id): bool;
 
 }
