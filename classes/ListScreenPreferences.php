25f32ec33dbc36165dfe69bc92ab6be7429c6712
David Mosterd <david@codepress.nl>
2023-09-22 10:52:08 +0200

    preference keys

---
+++ classes/ListScreenPreferences.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AC;
+
+interface ListScreenPreferences
+{
+
+    public const SHARED_SEGMENTS = 'shared_segments';
+    public const FILTER_SEGMENT = 'filter_segment';
+
+}
