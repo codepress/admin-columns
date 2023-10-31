d0afef617110e8639153444841e64b2e075ba3c3
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2023-10-31 13:24:40 +0100

    Create AcSkeleton.svelte

---
+++ src/ui/element/AcSkeleton.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let count: number = 1;
+</script>
+<style>
+	.acui-skeleton {
+		display: inline-flex;
+		flex-direction: column;
+		width: 100%;
+		vertical-align: middle;
+	}
+	.acui-skeleton-item {
+		background: #eee;
+	}
+</style>
+<div class="acui-skeleton">
+	{#each Array( count ) as i}
+		<div class="acui-skeleton-ui">
+		</div>
+	{/each}
+</div>
