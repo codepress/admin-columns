c36118e96345b5bd3f8ceeeb5b0f5d35989a9559
David Mosterd <david@codepress.nl>
2023-11-24 16:38:40 +0100

    en nu?

--- src/js/expression/and-specification.ts
+++ src/js/expression/and-specification.ts
@@ -1,6 +1,23 @@
-import AggregateSpecification from "./aggregate-specification";
+import Specification from "./specification";
+import NotSpecification from "./not-specification";
+import OrSpecification from "./or-specification";
 
-export default class AndSpecification extends AggregateSpecification {
+export default class AndSpecification implements Specification {
+
+    constructor(private specifications: Array<Specification>) {
+    }
+
+    andSpecification(specification: Specification): Specification {
+        return new AndSpecification([specification, this]);
+    }
+
+    not(): Specification {
+        return new NotSpecification(this);
+    }
+
+    orSpecification(specification: Specification): Specification {
+        return new OrSpecification([specification, this]);
+    }
 
     isSatisfiedBy(value: string): boolean {
         this.specifications.forEach(specification => {
--- src/js/expression/or-specification.ts
+++ src/js/expression/or-specification.ts
@@ -1,6 +1,23 @@
-import AggregateSpecification from "./aggregate-specification";
+import Specification from "./specification";
+import NotSpecification from "./not-specification";
+import AndSpecification from "./and-specification";
 
-export default class OrSpecification extends AggregateSpecification {
+export default class OrSpecification implements Specification {
+
+    constructor(private specifications: Array<Specification>) {
+    }
+
+    andSpecification(specification: Specification): Specification {
+        return new AndSpecification([specification, this]);
+    }
+
+    not(): Specification {
+        return new NotSpecification(this);
+    }
+
+    orSpecification(specification: Specification): Specification {
+        return new OrSpecification([specification, this]);
+    }
 
     isSatisfiedBy(value: string): boolean {
         this.specifications.forEach(specification => {
