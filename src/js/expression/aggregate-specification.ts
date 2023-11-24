edfe1963ffe02e534f69a2247a51dcf3a7f2fb3a
David Mosterd <david@codepress.nl>
2023-11-24 16:35:53 +0100

    ts

--- src/js/expression/aggregate-specification.ts
+++ src/js/expression/aggregate-specification.ts
@@ -1,10 +1,25 @@
-import BaseSpecification from "./base-specification";
 import Specification from "./specification";
+import AndSpecification from "./and-specification";
+import NotSpecification from "./not-specification";
+import OrSpecification from "./or-specification";
 
-export default abstract class AggregateSpecification extends BaseSpecification {
+export default abstract class AggregateSpecification implements Specification {
 
-    protected constructor(protected specifications: Array<Specification>) {
-        super();
+    constructor(protected specifications: Array<Specification>) {
     }
+
+    andSpecification(specification: Specification): Specification {
+        return new AndSpecification([specification, this]);
+    }
+
+    not(): Specification {
+        return new NotSpecification(this);
+    }
+
+    orSpecification(specification: Specification): Specification {
+        return new OrSpecification([specification, this]);
+    }
+
+    abstract isSatisfiedBy(value: string): boolean;
 
 }
