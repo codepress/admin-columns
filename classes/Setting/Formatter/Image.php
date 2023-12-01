d538eab49295d8e68158d70bd723faa9720f0d70
David Mosterd <david@codepress.nl>
2023-12-01 18:22:04 +0100

    conflict

---
+++ classes/Setting/Formatter/Image.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AC\Formatter;
+
+use AC\Formatter;
+use AC\Setting\Type\Value;
+
+class Image implements Formatter
+{
+
+    /**
+     * @var string|array
+     */
+    private $size;
+
+    private $skip_image_check;
+
+    public function __construct($size, bool $skip_image_check = false)
+    {
+        $this->size = $size;
+        $this->skip_image_check = $skip_image_check;
+    }
+
+    public function format(Value $value): Value
+    {
+        return $value->with_value(
+            ac_helper()->image->get_image($value, $this->size, $this->skip_image_check)
+        );
+    }
+
+}
