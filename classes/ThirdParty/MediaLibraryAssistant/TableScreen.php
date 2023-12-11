d4d1bad74597b82499f928c07e1c4dc3c8204d27
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2023-12-11 13:55:24 +0100

    WIP MLA

--- classes/ThirdParty/MediaLibraryAssistant/TableScreen.php
+++ classes/ThirdParty/MediaLibraryAssistant/TableScreen.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace AC\ThirdParty\MediaLibraryAssistant;
 
 use AC;
-use AC\Column;
 use AC\MetaType;
 use AC\Type\Labels;
 use AC\Type\ListKey;
@@ -16,34 +15,12 @@ use MLACore;
 class TableScreen extends AC\TableScreen implements AC\ListScreen\ListTable, AC\TableScreen\MetaType
 {
 
-    public function __construct()
+    public function __construct(array $columns = [])
     {
         parent::__construct(
             new ListKey('mla-media-assistant'),
             'media_page_' . MLACore::ADMIN_PAGE_SLUG,
-            [
-                Column\CustomField::class,
-                Column\Actions::class,
-                Column\Post\Slug::class,
-                Column\Post\TitleRaw::class,
-                Column\Media\Album::class,
-                Column\Media\Artist::class,
-                Column\Media\Author::class,
-                Column\Media\AvailableSizes::class,
-                Column\Media\Date::class,
-                Column\Media\Dimensions::class,
-                Column\Media\ExifData::class,
-                Column\Media\FileMetaAudio::class,
-                Column\Media\FileMetaVideo::class,
-                Column\Media\FileSize::class,
-                Column\Media\Height::class,
-                Column\Media\Image::class,
-                Column\Media\Menu::class,
-                Column\Media\Preview::class,
-                Column\Media\Title::class,
-                Column\Media\VideoPlayer::class,
-                Column\Media\Width::class,
-            ]
+            $columns
         );
     }
 
--- classes/ThirdParty/MediaLibraryAssistant/TableScreenFactory.php
+++ classes/ThirdParty/MediaLibraryAssistant/TableScreenFactory.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace AC\ThirdParty\MediaLibraryAssistant;
 
 use AC;
+use AC\Column;
 use AC\Type\ListKey;
 use MLACore;
 use WP_Screen;
@@ -14,7 +15,36 @@ class TableScreenFactory implements AC\TableScreenFactory
 
     public function create(ListKey $key): AC\TableScreen
     {
-        return new TableScreen();
+        return new TableScreen(
+            $this->get_columns()
+        );
+    }
+
+    protected function get_columns(): array
+    {
+        return [
+            Column\CustomField::class,
+            Column\Actions::class,
+            Column\Post\Slug::class,
+            Column\Post\TitleRaw::class,
+            Column\Media\Album::class,
+            Column\Media\Artist::class,
+            Column\Media\Author::class,
+            Column\Media\AvailableSizes::class,
+            Column\Media\Date::class,
+            Column\Media\Dimensions::class,
+            Column\Media\ExifData::class,
+            Column\Media\FileMetaAudio::class,
+            Column\Media\FileMetaVideo::class,
+            Column\Media\FileSize::class,
+            Column\Media\Height::class,
+            Column\Media\Image::class,
+            Column\Media\Menu::class,
+            Column\Media\Preview::class,
+            Column\Media\Title::class,
+            Column\Media\VideoPlayer::class,
+            Column\Media\Width::class,
+        ];
     }
 
     public function can_create(ListKey $key): bool
@@ -24,7 +54,9 @@ class TableScreenFactory implements AC\TableScreenFactory
 
     public function create_from_wp_screen(WP_Screen $screen): AC\TableScreen
     {
-        return new TableScreen();
+        return new TableScreen(
+            $this->get_columns()
+        );
     }
 
     public function can_create_from_wp_screen(WP_Screen $screen): bool
