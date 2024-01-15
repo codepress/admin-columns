63a224b35f434bd27266c7144f0a62be9eb302e5
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2024-01-15 15:49:39 +0100

    Update Nonce.php

--- classes/Form/Nonce.php
+++ classes/Form/Nonce.php
@@ -1,44 +1,52 @@
 <?php
 
-declare( strict_types=1 );
+declare(strict_types=1);
 
 namespace AC\Form;
 
 use AC\Request;
 
-class Nonce {
+class Nonce
+{
 
-	private $action;
+    private $action;
 
-	private $name;
+    private $name;
 
-	public function __construct( string $action, string $name ) {
-		$this->action = $action;
-		$this->name = $name;
-	}
+    public function __construct(string $action, string $name)
+    {
+        $this->action = $action;
+        $this->name = $name;
+    }
 
-	public function get_action(): string {
-		return $this->action;
-	}
+    public function get_action(): string
+    {
+        return $this->action;
+    }
 
-	public function get_name(): string {
-		return $this->name;
-	}
+    public function get_name(): string
+    {
+        return $this->name;
+    }
 
-	public function create(): ?string {
-		return wp_create_nonce( $this->action ) ?: null;
-	}
+    public function create(): ?string
+    {
+        return wp_create_nonce($this->action) ?: null;
+    }
 
-	public function create_field(): string {
-		return wp_nonce_field( $this->action, $this->name, true, false );
-	}
+    public function create_field(): string
+    {
+        return wp_nonce_field($this->action, $this->name, true, false);
+    }
 
-	public function verify_nonce( string $nonce ): bool {
-		return (bool) wp_verify_nonce( $nonce, $this->action );
-	}
+    public function verify_nonce(string $nonce): bool
+    {
+        return (bool)wp_verify_nonce($nonce, $this->action);
+    }
 
-	public function verify( Request $request ): bool {
-		return $this->verify_nonce( $request->get( $this->name ) );
-	}
+    public function verify(Request $request): bool
+    {
+        return $this->verify_nonce($request->get($this->name, ''));
+    }
 
 }
