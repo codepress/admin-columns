efb76c5ee003e7914883551edd9093aa69d4c231
Tobias Schutter <tobias@codepress.nl>
2024-01-26 15:17:51 +0100

    wip

--- classes/Setting/Base/Setting.php
+++ classes/Setting/Base/Setting.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace AC\Setting\Base;
 
 use AC;
-use AC\Setting\Component;
 use AC\Setting\SettingTrait;
 use ACP\Expression\Specification;
 
--- classes/Settings/Column/Message.php
+++ classes/Settings/Column/Message.php
@@ -2,10 +2,6 @@
 
 namespace AC\Settings\Column;
 
-use AC;
-use AC\Setting\Component;
-use AC\Setting\Component\AttributeCollection;
-use AC\Setting\Component\AttributeFactory;
 use AC\Expression\Specification;
 use AC\Settings;
 
@@ -19,10 +15,11 @@ class Message extends Settings\Column
             'message',
             $label,
             '',
+            null,
             // TODO create a component
-            new Element('message', null, null, new AttributeCollection([
-                AttributeFactory::create_data('message', $message),
-            ]),
+            //            new Element('message', null, null, new AttributeCollection([
+            //                AttributeFactory::create_data('message', $message),
+            //            ]),
             $conditions
         );
     }
