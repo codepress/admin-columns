e3497ca1a958e6ec72a95a863dca8315769f2732
David Mosterd <david@codepress.nl>
2024-01-30 14:14:29 +0100

    string limit

--- classes/Settings/Column/StringLimitFactory.php
+++ classes/Settings/Column/StringLimitFactory.php
@@ -17,10 +17,18 @@ class StringLimitFactory implements SettingFactory
     public static function create(Config $config, Specification $specification = null): Column
     {
         return new StringLimit(
-            $config->has('string_limit') ? $config->get('string_limit') : 'word_limit',
+            $config->has('string_limit')
+                ? $config->get('string_limit')
+                : '',
             new SettingCollection([
-                (new CharacterLimitFactory())->create($config, StringComparisonSpecification::equal('character_limit')),
-                (new WordLimitFactory())->create($config, StringComparisonSpecification::equal('word_limit')),
+                CharacterLimitFactory::create(
+                    $config,
+                    StringComparisonSpecification::equal('character_limit')
+                ),
+                WordLimitFactory::create(
+                    $config,
+                    StringComparisonSpecification::equal('word_limit')
+                ),
             ])
         );
     }
