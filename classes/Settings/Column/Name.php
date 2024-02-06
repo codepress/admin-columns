eb3c73b64d6419206142ff2efc1088f80a5e1d67
David Mosterd <david@codepress.nl>
2024-02-06 17:02:46 +0100

    todo

--- classes/Settings/Column/Name.php
+++ classes/Settings/Column/Name.php
@@ -6,6 +6,7 @@ namespace AC\Settings\Column;
 
 use AC\Setting\Component\Input;
 
+// TODO this seems a bit of a useless class? A dedicated factory can just create this
 class Name extends Input
 {
 
