9202a1f1999f6a985ba89a7023d89d92528dc187
David Mosterd <david@codepress.nl>
2024-02-27 09:22:11 +0100

    naming on separator

--- classes/Settings/Column/Separator.php
+++ classes/Settings/Column/Separator.php
@@ -1,13 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 namespace AC\Settings\Column;
 
 use AC;
 use AC\Expression\Specification;
 use AC\Settings;
 
-class Separator extends Settings\Control
+final class Separator extends Settings\Control
 {
+
+    public const DEFAULT = '';
+    public const COMMA = 'comma';
+    public const HORIZONTAL_RULE = 'horizontal_rule';
+    public const NEW_LINE = 'newline';
+    public const NONE = 'none';
+    public const WHITE_SPACE = 'white_space';
 
     public function __construct(string $separator, Specification $specification = null)
     {
@@ -15,12 +24,12 @@ class Separator extends Settings\Control
             AC\Setting\Component\Input\OptionFactory::create_select(
                 'separator',
                 AC\Setting\Component\OptionCollection::from_array([
-                    ''                => __('Default', 'codepress-admin-columns'),
-                    'comma'           => __('Comma Separated', 'codepress-admin-columns'),
-                    'horizontal_rule' => __('Horizontal Rule', 'codepress-admin-columns'),
-                    'newline'         => __('New Line', 'codepress-admin-columns'),
-                    'none'            => __('None', 'codepress-admin-columns'),
-                    'white_space'     => __('Whitespace', 'codepress-admin-columns'),
+                    self::DEFAULT         => __('Default', 'codepress-admin-columns'),
+                    self::COMMA           => __('Comma Separated', 'codepress-admin-columns'),
+                    self::HORIZONTAL_RULE => __('Horizontal Rule', 'codepress-admin-columns'),
+                    self::NEW_LINE        => __('New Line', 'codepress-admin-columns'),
+                    self::NONE            => __('None', 'codepress-admin-columns'),
+                    self::WHITE_SPACE     => __('Whitespace', 'codepress-admin-columns'),
                 ]),
                 $separator
             ),
