7503be287f68f5de1d6bdc1c7a684de822174b50
David Mosterd <david@codepress.nl>
2024-03-18 12:35:15 +0100

    beforemore

--- classes/ColumnFactory/Post/BeforeMoreFactory.php
+++ classes/ColumnFactory/Post/BeforeMoreFactory.php
@@ -8,22 +8,27 @@ use AC\Column\ColumnFactory;
 use AC\Setting\ComponentCollection;
 use AC\Setting\ComponentFactory\WordLimit;
 use AC\Setting\ComponentFactoryRegistry;
-use AC\Setting\Config;
-use AC\Setting\Formatter;
-use AC\Setting\Formatter\AggregateBuilderFactory;
 use AC\Setting\Formatter\Post\BeforeMoreContent;
 
-class BeforeMoreFactory extends ColumnFactory
+final class BeforeMoreFactory extends ColumnFactory
 {
 
+    private $word_limit_factory;
+
     public function __construct(
-        AggregateBuilderFactory $aggregate_formatter_builder_factory,
         ComponentFactoryRegistry $component_factory_registry,
         WordLimit $word_limit_factory
     ) {
-        parent::__construct($aggregate_formatter_builder_factory, $component_factory_registry);
+        parent::__construct($component_factory_registry);
 
-        $this->add_component_factory($word_limit_factory);
+        $this->word_limit_factory = $word_limit_factory;
+    }
+
+    protected function add_component_factories(): void
+    {
+        parent::add_component_factories();
+
+        $this->add_component_factory($this->word_limit_factory);
     }
 
     public function get_type(): string
@@ -36,11 +41,11 @@ class BeforeMoreFactory extends ColumnFactory
         return __('More Tag', 'codepress-admin-columns');
     }
 
-    protected function create_formatter_builder(
-        ComponentCollection $components,
-        Config $config
-    ): Formatter\AggregateBuilder {
-        return parent::create_formatter_builder($components, $config)->prepend(new BeforeMoreContent());
+    protected function get_formatters(ComponentCollection $components, $formatters = []): array
+    {
+        return parent::get_formatters($components, [
+            new BeforeMoreContent(),
+        ]);
     }
 
 }
