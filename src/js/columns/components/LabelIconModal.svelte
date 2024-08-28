804b6edd41eed55ab56989994b1fcf009fee25a5
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2024-08-28 16:38:15 +0200

    Label input

---
+++ src/js/columns/components/LabelIconModal.svelte
@@ -0,0 +1,89 @@
+<script lang="ts">
+    import dashicons from '../config/dashicons.json';
+    import AcModal from "ACUi/AcModal.svelte";
+    import AcButton from "ACUi/element/AcButton.svelte";
+    import {getColumnSettingsTranslation} from "../utils/global";
+    import {createEventDispatcher} from "svelte";
+
+    export let value: string;
+
+    type iconsList = Array<{ name: string, items: string[] }>
+
+    const icons: iconsList = dashicons
+    const i18n = getColumnSettingsTranslation();
+    const dispatch = createEventDispatcher();
+
+    let selectedIcon: string;
+    let searchQuery: string = '';
+
+
+    let filteredIcons: iconsList = icons;
+
+    const searchIcons = () => {
+        let filtered: iconsList = [];
+
+        icons.forEach(g => {
+            let groupIcons: string[] = [];
+            g.items.forEach(i => {
+                if (i.includes(searchQuery.toLowerCase())) {
+                    groupIcons.push(i)
+                }
+            });
+            if (groupIcons.length > 0) {
+                filtered.push({name: g.name, items: groupIcons})
+            }
+        });
+
+        filteredIcons = filtered;
+    }
+
+    const applyIcon = () => {
+        value = `<span class="dashicons dashicons-${selectedIcon}"></span>`
+        dispatch('change', value);
+    }
+
+    const selectIcon = (dashicon: string) => {
+        selectedIcon = dashicon;
+    }
+
+</script>
+
+
+<AcModal visible className="acui2 -iconpicker -xl" appendToBody on:close>
+	<span slot="header">
+		<h2>Select Icon</h2>
+	</span>
+	<div slot="content">
+		{#each filteredIcons as icongroup}
+			<h3 class="acu-font-normal acu-mt-[0] acu-mb-1">{icongroup.name}</h3>
+			<div class="ac-ipicker__group">
+				{#each icongroup.items as dashicon}
+					<button
+						class="ac-ipicker__icon"
+						data-dashicon={dashicon}
+						tabindex="0"
+						class:active={dashicon === selectedIcon}
+						on:click={() =>selectIcon(dashicon)}
+					>
+						<span class="dashicons dashicons-{dashicon} acu-w-[40px] acu-h-[40px] acu-text-[28px]"></span>
+					</button>
+				{/each}
+			</div>
+		{/each}
+	</div>
+	<div slot="footer" class="acu-flex acu-items-center">
+		<div class="acu-flex-grow">
+			<input type="search" style="width:100%" placeholder={i18n.global.search} bind:value={searchQuery}
+				on:input={searchIcons}>
+		</div>
+		<div class="acu-flex acu-w-[60px] acu-items-center acu-justify-center">
+			{#if selectedIcon}
+				<span class="dashicons dashicons-{selectedIcon}"></span>
+			{/if}
+		</div>
+		<div class="acp-iconmodal-footer__action">
+			<AcButton on:click={applyIcon} type="primary">{i18n.global.select}</AcButton>
+		</div>
+
+	</div>
+</AcModal>
--- src/js/columns/components/settings/input/LabelInput.svelte
+++ src/js/columns/components/settings/input/LabelInput.svelte
@@ -1,55 +1,27 @@
 <script lang="ts">
     import dashicons from '../../../config/dashicons.json';
     import AcInputGroup from "ACUi/acui-form/AcInputGroup.svelte";
-    import AcModal from "ACUi/AcModal.svelte";
-    import AcButton from "ACUi/element/AcButton.svelte";
     import {getColumnSettingsTranslation} from "../../../utils/global";
+    import LabelIconModal from "../../LabelIconModal.svelte";
 
     export let value: string;
     export let disabled: boolean = false;
 
-    type iconsList = Array<{ name: string, items: string[] }>
-
-    const icons: iconsList = dashicons
-    const i18n = getColumnSettingsTranslation();
-
     let showIconModal: boolean = false;
-    let selectedIcon: string;
-    let searchQuery: string = '';
-
-
-    let filteredIcons: iconsList = icons;
-
-    const searchIcons = () => {
-        let filtered: iconsList = [];
-
-        icons.forEach(g => {
-            let groupIcons: string[] = [];
-            g.items.forEach(i => {
-                if (i.includes(searchQuery.toLowerCase())) {
-                    groupIcons.push(i)
-                }
-            });
-            if (groupIcons.length > 0) {
-                filtered.push({name: g.name, items: groupIcons})
-            }
-        });
-
-        filteredIcons = filtered;
-    }
 
     const addIcon = () => {
         showIconModal = true;
     }
 
-    const applyIcon = () => {
-        value = `<span class="dashicons dashicons-${selectedIcon}"></span>`
+    const handleIconSelect = (event: CustomEvent) => {
+        value = event.detail;
         showIconModal = false;
-    }
+	}
 
-    const selectIcon = (dashicon: string) => {
-        selectedIcon = dashicon;
-    }
+    const closeModal = () => {
+        showIconModal = false;
+	}
+
 
 </script>
 
@@ -60,39 +32,6 @@
 	</div>
 
 	{#if showIconModal && !disabled}
-		<AcModal visible on:close={()=>showIconModal = false} --modalWidth="960px" className="-iconpicker">
-			<span slot="header">{i18n.settings.label.select_label}</span>
-			<div slot="content">
-				{#each filteredIcons as icongroup}
-					<h3>{icongroup.name}</h3>
-					{#each icongroup.items as dashicon}
-						<button
-							class="acp-iconpicker__icon"
-							data-dashicon={dashicon}
-							tabindex="0"
-							on:click={() =>selectIcon(dashicon)}
-						>
-							<span class="dashicons dashicons-{dashicon}"></span>
-						</button>
-					{/each}
-
-				{/each}
-
-			</div>
-			<div slot="footer" class="acp-iconmodal-footer">
-				<div class="acp-iconmodal-footer__input">
-					<input type="search" placeholder={i18n.global.search} bind:value={searchQuery} on:input={searchIcons}>
-				</div>
-				<div class="acp-iconmodal-footer__selection">
-					{#if selectedIcon}
-						<span class="dashicons dashicons-{selectedIcon}"></span>
-					{/if}
-				</div>
-				<div class="acp-iconmodal-footer__action">
-					<AcButton on:click={applyIcon} type="primary">{i18n.global.select}</AcButton>
-				</div>
-
-			</div>
-		</AcModal>
+		<LabelIconModal value="" on:change={ handleIconSelect } on:close={closeModal}/>
 	{/if}
 </AcInputGroup>
