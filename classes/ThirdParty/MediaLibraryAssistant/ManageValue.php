1c6f05c16efe7e50b8f3277af4c70f9d30ce7719
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2024-09-11 12:59:23 +0200

    Original

--- classes/ThirdParty/MediaLibraryAssistant/ManageValue.php
+++ classes/ThirdParty/MediaLibraryAssistant/ManageValue.php
@@ -9,6 +9,13 @@ use AC;
 class ManageValue extends AC\Table\ManageValue
 {
 
+    private $renderable;
+
+    public function __construct(AC\Table\ColumnRenderable $renderable)
+    {
+        $this->renderable = $renderable;
+    }
+
     public function register(): void
     {
         add_filter('mla_list_table_column_default', [$this, 'render_value'], 100, 3);
@@ -17,8 +24,7 @@ class ManageValue extends AC\Table\ManageValue
     public function render_value($value, $post, $column_name)
     {
         if (is_null($value)) {
-            // TODO
-            $value = $this->render_cell((string)$column_name, (int)$post->ID);
+            $value = $this->renderable->render($column_name, (int)$post->ID);
         }
 
         return $value;
