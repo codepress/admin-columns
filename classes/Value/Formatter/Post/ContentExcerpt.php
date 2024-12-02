1d1a04a132d45a0c45f8b35af5f1d237ed31ef16
Tobias Schutter <tobias@codepress.nl>
2024-12-02 14:47:57 +0100

    Update ContentExcerpt.php

--- classes/Value/Formatter/Post/ContentExcerpt.php
+++ classes/Value/Formatter/Post/ContentExcerpt.php
@@ -22,23 +22,19 @@ class ContentExcerpt implements Formatter
         $excerpt = ac_helper()->post->excerpt($post->ID);
 
         if ( ! $excerpt) {
-            return $value;
+            return throw ValueNotFoundException::from_id($value->get_id());
         }
 
         if ($post->post_excerpt) {
             return $value->with_value($excerpt);
         }
 
-        if ($post->post_content) {
-            $tooltip = ac_helper()->html->tooltip(
-                ac_helper()->icon->dashicon(['icon' => 'media-text', 'class' => 'gray']),
-                __('Excerpt is missing.') . ' ' . __('Current excerpt is generated from the content.')
-            );
+        $tooltip = ac_helper()->html->tooltip(
+            ac_helper()->icon->dashicon(['icon' => 'media-text', 'class' => 'gray']),
+            __('Excerpt is missing.') . ' ' . __('Current excerpt is generated from the content.')
+        );
 
-            return $value->with_value($tooltip . $excerpt);
-        }
-
-        return $value;
+        return $value->with_value($tooltip . $excerpt);
     }
 
 }
