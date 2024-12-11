a2e852747ce5a045ce1da7cc10b28942ea24f49a
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2024-12-11 13:25:39 +0100

    Hooks todo

--- classes/Deprecated/Hooks.php
+++ classes/Deprecated/Hooks.php
@@ -26,25 +26,45 @@ class Hooks
     private function get_filters(): array
     {
         $hooks = [
-            new Filter('cac/headings/label', '3.0', 'cac-columns-custom'),
-            new Filter('cac/column/meta/value', '3.0', 'cac-column-meta-value'),
-            new Filter('cac/column/meta/types', '3.0', 'cac-column-meta-types'),
-            new Filter('cac/settings/tabs', '3.0', 'cac-settings-tabs'),
-            new Filter('cac/editable/is_column_editable', '3.0', 'cac-editable-is_column_editable'),
-            new Filter('cac/editable/editables_data', '3.0', 'cac-editable-editables_data'),
-            new Filter('cac/editable/options', '3.0', 'cac-editable-editables_data'),
-            new Filter('cac/inline-edit/ajax-column-save/value', '3.0', 'cac-inline-edit-ajax-column-save-value'),
-            new Filter('cac/addon/filtering/options', '3.0', 'cac-addon-filtering-options'),
-            new Filter('cac/addon/filtering/dropdown_top_label', '3.0', 'cac-addon-filtering-dropdown_top_label'),
-            new Filter('cac/addon/filtering/taxonomy/terms_args', '3.0', 'cac-addon-filtering-taxonomy-terms_args'),
-            new Filter('cac/addon/filtering/dropdown_empty_option', '3.0', 'cac-addon-filtering-taxonomy-terms_args'),
-            new Filter('cac/column/actions/action_links', '3.0', 'cac-column_actions-action_links'),
-            new Filter('cac/acf/format_acf_value', '3.0', 'cac-acf-format_acf_value'),
-            new Filter('cac/addon/filtering/taxonomy/terms_args', '3.0'),
-            new Filter('cac/column/meta/use_text_input', '3.0'),
-            new Filter('cac/hide_renewal_notice', '3.0'),
-            new Filter('acp/network_settings/groups', '3.4'),
-            new Filter('acp/settings/groups', '3.4'),
+            new Filter('ac/column/value', '5.0', 'ac/v2/column/value'),
+            new Filter('ac/column/value/sanitize', '5.0', 'ac/v2/column/value/sanitize'),
+            new Filter('ac/column_types', '5.0', 'ac/v2/column_types'),
+
+            //            TODO replace these hooks that contain $column
+            //            ac/column/audio_player/valid_mime_types
+            //            ac/headings/label
+            //            ac/list_screen/preferences
+            //            ac/headings
+            //            ac/column/label  --> See WPML.php as internal usage
+            //            ac/column/separator
+            //            ac/column/video_player/valid_mime_types
+            //            ac/column/settings/column_types
+            //            ac/column_group
+
+            // TODO More hooks to check
+            //            ac/column/custom_field/use_text_input
+            //            ac/column/custom_field/field_types
+
+            // TODO remove
+            //            new Filter('cac/headings/label', '3.0', 'cac-columns-custom'),
+            //            new Filter('cac/column/meta/value', '3.0', 'cac-column-meta-value'),
+            //            new Filter('cac/column/meta/types', '3.0', 'cac-column-meta-types'),
+            //            new Filter('cac/settings/tabs', '3.0', 'cac-settings-tabs'),
+            //            new Filter('cac/editable/is_column_editable', '3.0', 'cac-editable-is_column_editable'),
+            //            new Filter('cac/editable/editables_data', '3.0', 'cac-editable-editables_data'),
+            //            new Filter('cac/editable/options', '3.0', 'cac-editable-editables_data'),
+            //            new Filter('cac/inline-edit/ajax-column-save/value', '3.0', 'cac-inline-edit-ajax-column-save-value'),
+            //            new Filter('cac/addon/filtering/options', '3.0', 'cac-addon-filtering-options'),
+            //            new Filter('cac/addon/filtering/dropdown_top_label', '3.0', 'cac-addon-filtering-dropdown_top_label'),
+            //            new Filter('cac/addon/filtering/taxonomy/terms_args', '3.0', 'cac-addon-filtering-taxonomy-terms_args'),
+            //            new Filter('cac/addon/filtering/dropdown_empty_option', '3.0', 'cac-addon-filtering-taxonomy-terms_args'),
+            //            new Filter('cac/column/actions/action_links', '3.0', 'cac-column_actions-action_links'),
+            //            new Filter('cac/acf/format_acf_value', '3.0', 'cac-acf-format_acf_value'),
+            //            new Filter('cac/addon/filtering/taxonomy/terms_args', '3.0'),
+            //            new Filter('cac/column/meta/use_text_input', '3.0'),
+            //            new Filter('cac/hide_renewal_notice', '3.0'),
+            //            new Filter('acp/network_settings/groups', '3.4'),
+            //            new Filter('acp/settings/groups', '3.4'),
         ];
 
         $hooks[] = new Filter('cac/columns/custom', '3.0', 'cac-columns-custom');
