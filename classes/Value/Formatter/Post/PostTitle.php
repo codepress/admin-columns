3f71962e900a6143afb516325690ff84fde36205
Tobias Schutter <tobias@codepress.nl>
2025-01-21 13:42:40 +0100

    Update PostTitle.php

--- classes/Value/Formatter/Post/PostTitle.php
+++ classes/Value/Formatter/Post/PostTitle.php
@@ -4,17 +4,34 @@ declare(strict_types=1);
 
 namespace AC\Value\Formatter\Post;
 
+use AC\Exception\ValueNotFoundException;
 use AC\Setting\Formatter;
 use AC\Type\Value;
+use WP_Post;
 
 class PostTitle implements Formatter
 {
 
     public function format(Value $value): Value
     {
+        $post = get_post($value->get_id());
+
+        if ( ! $post) {
+            throw ValueNotFoundException::from_id($value->get_id());
+        }
+
         return $value->with_value(
-            get_the_title($value->get_id())
+            $this->get_title($post)
         );
+    }
+
+    private function get_title(WP_Post $post): string
+    {
+        if ('attachment' === $post->post_type) {
+            return ac_helper()->image->get_file_name($post->ID) ?: '';
+        }
+
+        return get_the_title($post);
     }
 
 }
