754bd625e9ab38375d15249591f6fc479fcc5b43
David Mosterd <david@codepress.nl>
2025-03-31 15:46:27 +0200

    untested, but good enough to work on: better factories

--- classes/Column/ColumnFactory.php
+++ classes/Column/ColumnFactory.php
@@ -3,15 +3,63 @@
 namespace AC\Column;
 
 use AC\Column;
+use AC\Setting\ComponentCollection;
 use AC\Setting\Config;
+use AC\Setting\FormatterCollection;
 
-interface ColumnFactory
+abstract class ColumnFactory
 {
 
-    public function create(Config $config): Column;
+    public function create(Config $config): Column
+    {
+        return new Base(
+            $this->get_column_type(),
+            $this->get_label(),
+            $this->get_default_settings($config)
+                 ->merge($this->get_settings($config)),
+            ColumnIdFactory::createFromConfig($config),
+            $this->get_formatters($config),
+            $this->get_group()
+        );
+    }
 
-    public function get_column_type(): string;
+    abstract public function get_column_type(): string;
 
-    public function get_label(): string;
+    abstract public function get_label(): string;
+
+    protected function get_default_settings(Config $config): ComponentCollection
+    {
+        return new ComponentCollection();
+    }
+
+    protected function get_settings(Config $config): ComponentCollection
+    {
+        return new ComponentCollection();
+    }
+
+    protected function get_formatters(Config $config): FormatterCollection
+    {
+        return $this->get_formatters_from_settings(
+            $this->get_settings($config)
+        );
+    }
+
+    private function get_formatters_from_settings(ComponentCollection $settings): FormatterCollection
+    {
+        $formatters = new FormatterCollection();
+
+        foreach ($settings as $setting) {
+            foreach ($setting->get_formatters() as $formatter) {
+                $formatters->add($formatter);
+            }
+        }
+
+        return $formatters;
+    }
+
+    protected function get_group(): ?string
+    {
+        return null;
+    }
 
 }
