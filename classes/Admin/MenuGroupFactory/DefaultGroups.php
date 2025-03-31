a4faa489a3b0897a6ffceba1710ea3a4262a524c
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2025-03-31 11:21:12 +0200

    Update DefaultGroups.php

--- classes/Admin/MenuGroupFactory/DefaultGroups.php
+++ classes/Admin/MenuGroupFactory/DefaultGroups.php
@@ -18,22 +18,26 @@ class DefaultGroups implements MenuGroupFactory
                 $post_type = get_post_type_object((string)$table_screen->get_post_type());
 
                 if ($post_type && in_array($post_type->name, ['post', 'page'], true)) {
-                    return new MenuGroup('default', __('Default'), 1, 'dashicons-wordpress');
+                    return $this->get_default_group();
                 }
 
                 if ($post_type && $post_type->show_in_menu) {
-                    return new MenuGroup('post', __('Post Types'), 5, 'cpacicon-gf-article');
+                    return new MenuGroup('post', __('Post Types', 'codepress-admin-columns'), 5, 'cpacicon-gf-article');
                 }
 
                 return new MenuGroup(
                     'post-hidden',
-                    sprintf('%s (%s)', __('Custom Post Types'), __('hidden', 'codepress-admin-columns')),
+                    sprintf(
+                        '%s (%s)',
+                        __('Custom Post Types', 'codepress-admin-columns'),
+                        __('hidden', 'codepress-admin-columns')
+                    ),
                     30
                 );
             case $table_screen instanceof TableScreen\User :
             case $table_screen instanceof TableScreen\Media :
             case $table_screen instanceof TableScreen\Comment :
-                return new MenuGroup('default', __('Default'), 1, 'dashicons-wordpress');
+                return $this->get_default_group();
             default :
                 return null;
         }
