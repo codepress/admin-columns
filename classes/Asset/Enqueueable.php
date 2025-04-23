52f8dd8785fe22bda8613e0c3e7d3813caa2f42c
Tobias Schutter <tobias@codepress.nl>
2025-04-23 12:46:14 +0200

    Script templates

--- classes/Asset/Enqueueable.php
+++ classes/Asset/Enqueueable.php
@@ -7,20 +7,11 @@ use AC\Asset\Location\Absolute;
 abstract class Enqueueable
 {
 
-    /**
-     * @var string
-     */
-    protected $handle;
+    protected string $handle;
 
-    /**
-     * @var Absolute|null
-     */
-    protected $location;
+    protected ?Absolute $location;
 
-    /**
-     * @var string[]
-     */
-    protected $dependencies;
+    protected array $dependencies;
 
     public function __construct(string $handle, Absolute $location = null, array $dependencies = [])
     {
