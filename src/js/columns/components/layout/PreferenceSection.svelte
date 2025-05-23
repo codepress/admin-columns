291a4287747cc0ba66ded2c063e2f67cbb32e608
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2025-05-23 14:48:13 +0200

    Panel and modern CSS container classes

--- src/js/columns/components/layout/PreferenceSection.svelte
+++ src/js/columns/components/layout/PreferenceSection.svelte
@@ -4,14 +4,15 @@
 </script>
 
 <div class="ac-listscreen-prefs-section acu-pb-8 acu-mb-8 last:acu-mb-[0] last:acu-pb-[0] acu-border-0 acu-border-b acu-border-solid acu-border-ui-border last:acu-border-b-0">
-	<div class="ac-listscreen-prefs-setting xl:acu-flex">
-		<div class="ac-listscreen-prefs-setting__label xl:acu-w-[240px] lg:acu-mr-6 rtl:acu-mr-[0] rtl:acu-ml-6">
+	<div class="ac-listscreen-prefs-setting @[650px]/panel:acu-flex">
+		<div
+			class="ac-listscreen-prefs-setting__label @[650px]:acu-w-[240px] @[650px]:acu-mr-6 rtl:acu-mr-[0] rtl:acu-ml-6">
 			<h3 class="acu-mt-[0] acu-mb-2 acu-text-base acu-font-semibold">{label}</h3>
 			{#if description}
 				<p>{description}</p>
 			{/if}
 		</div>
-		<div class="ac-listscreen-prefs-setting__body acu-flex-auto acu-flex-grow">
+		<div class="ac-listscreen-prefs-setting__body acu-flex-auto acu-flex-grow acu-@container/setting">
 			<slot></slot>
 		</div>
 	</div>
--- src/js/columns/components/layout/PreferenceSettingItem.svelte
+++ src/js/columns/components/layout/PreferenceSettingItem.svelte
@@ -2,8 +2,8 @@
     export let label: string = '';
 </script>
 
-<div class="ac-listscreen-prefs-item md:acu-flex acu-pb-4 acu-mb-4 acu-border-0 acu-border-solid lg::acu-border-b acu-border-ui-border">
-	<div class="acu-w-1/3 acu-font-bold acu-mb-2">{label}
+<div class="ac-listscreen-prefs-item @[500px]/setting:acu-flex acu-pb-4 acu-mb-4 acu-border-0 acu-border-solid lg::acu-border-b acu-border-ui-border">
+	<div class="@[500px]/setting:acu-w-1/3 acu-font-bold acu-mb-2">{label}
 		<slot name="label"></slot>
 	</div>
 	<div class="acu-flex-grow">
