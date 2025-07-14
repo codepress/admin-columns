affa262d0309181298157b5506671b0d0447e5e2
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2025-07-14 15:53:52 +0200

    Styling for CF

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -50,6 +50,8 @@ module.exports = {
 			'ui-border': 'var(--acui-border)',
 			'link': 'var(--ac-link)',
 			'link-hover': 'var(--ac-link-hover)',
+			'acbutton': 'var(--ac-button-color)',
+			'acbutton-hover': 'var(--ac-button-hover)',
 			'notification-red': 'var(--ac-notification-red)',
 			'notification-blue': 'var(--ac-notification-blue)'
 		},
