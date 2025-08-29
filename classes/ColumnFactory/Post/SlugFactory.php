285d519f19e8e7acc4bad8c8d9d3bbad26c1b5a4
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2025-08-29 15:01:22 +0200

    Update SlugFactory.php

--- classes/ColumnFactory/Post/SlugFactory.php
+++ classes/ColumnFactory/Post/SlugFactory.php
@@ -3,26 +3,12 @@
 namespace AC\ColumnFactory\Post;
 
 use AC\Column\BaseColumnFactory;
-use AC\Setting\ComponentCollection;
-use AC\Setting\ComponentFactory\CharacterLimit;
 use AC\Setting\Config;
-use AC\Setting\DefaultSettingsBuilder;
 use AC\Setting\FormatterCollection;
 use AC\Value\Formatter\Post\Slug;
 
 class SlugFactory extends BaseColumnFactory
 {
-
-    private $character_limit;
-
-    public function __construct(
-        DefaultSettingsBuilder $default_settings_builder,
-        CharacterLimit $character_limit
-    ) {
-        parent::__construct($default_settings_builder);
-
-        $this->character_limit = $character_limit;
-    }
 
     public function get_label(): string
     {
@@ -32,13 +18,6 @@ class SlugFactory extends BaseColumnFactory
     public function get_column_type(): string
     {
         return 'column-slug';
-    }
-
-    protected function get_settings(Config $config): ComponentCollection
-    {
-        return new ComponentCollection([
-            $this->character_limit->create($config),
-        ]);
     }
 
     protected function get_formatters(Config $config): FormatterCollection
