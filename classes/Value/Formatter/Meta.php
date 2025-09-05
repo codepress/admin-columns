58f8f2317a98191bf66d736c477232a40b5a8544
Tobias Schutter <tobias@codepress.nl>
2025-09-05 10:32:48 +0200

    Update Meta.php

--- classes/Value/Formatter/Meta.php
+++ classes/Value/Formatter/Meta.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace AC\Value\Formatter;
 
+use AC\Exception\ValueNotFoundException;
 use AC\MetaType;
 use AC\Setting\Formatter;
 use AC\Type\Value;
@@ -23,6 +24,10 @@ class Meta implements Formatter
 
     public function format(Value $value): Value
     {
+        if ( ! $this->meta_key) {
+            throw ValueNotFoundException::from_id($value->get_id());
+        }
+
         return $value->with_value(
             get_metadata(
                 (string)$this->meta_type,
