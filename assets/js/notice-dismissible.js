4e2c904e6bc8ae6be4be0a3a827b17cc61192e1b
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2025-09-22 15:01:37 +0200

    More Icons from Google

--- assets/js/notice-dismissible.js
+++ assets/js/notice-dismissible.js
@@ -130,7 +130,7 @@ module.exports = jQuery;
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
-// This entry needs to be wrapped in an IIFE because it needs to be isolated against other modules in the chunk.
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
 /*!**********************************!*\
   !*** ./js/notice-dismissible.ts ***!
--- src/packages/material-icons/src/icons.js
+++ src/packages/material-icons/src/icons.js
@@ -1,11 +1,13 @@
 export const icons = [
 	'add_circle',
+	'cancel',
 	'chevron_left',
 	'chevron_right',
 	'content_copy',
 	'download',
 	'delete',
 	'description',
+	'do_not_disturb_on',
 	'drag_indicator',
 	'edit',
 	'filter_list',
