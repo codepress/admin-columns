e805faea5bc9c0834f5e4bdb6dae1a421b530cd8
Tobias Schutter <tobias@codepress.nl>
2025-10-01 11:32:05 +0200

    Added prio to menu items

--- classes/Admin/View/Menu.php
+++ classes/Admin/View/Menu.php
@@ -10,9 +10,19 @@ class Menu extends View
 
     public function __construct(Admin\Menu $menu)
     {
-        parent::__construct(['menu_items' => $menu->get_items()]);
+        $items = $menu->get_items();
+        $this->sort_by_position($items);
+    
+        parent::__construct(['menu_items' => $items]);
 
         $this->set_template('admin/menu');
+    }
+
+    private function sort_by_position(array &$items): void
+    {
+        usort($items, function (Admin\Type\MenuItem $a, Admin\Type\MenuItem $b) {
+            return $a->get_position() <=> $b->get_position();
+        });
     }
 
 }
