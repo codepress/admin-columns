055fb9a665bcfeaf2026f069c1ecf90948514e31
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2025-11-28 15:30:19 +0100

    Fieldbuildebuilder

--- classes/Setting/ComponentFactory/FieldType/CountConfigurator.php
+++ classes/Setting/ComponentFactory/FieldType/CountConfigurator.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace AC\Setting\ComponentFactory\FieldType;
 
-use AC\Expression\StringComparisonSpecification;
-use AC\Setting;
 use AC\Setting\ComponentFactory\FieldTypeBuilder;
-use AC\Value\Formatter;
 
+// TODO Should this be part of a field type setting?
 class CountConfigurator implements FieldTypeConfigurator
 {
 
@@ -16,24 +14,6 @@ class CountConfigurator implements FieldTypeConfigurator
 
     public function configure(FieldTypeBuilder $builder): void
     {
-        $builder
-            ->add_option(self::TYPE, __('Number of Fields', 'codepress-admin-columns'), 'basic')
-            ->add_formatter(
-                self::TYPE,
-                function (Setting\Config $config, Setting\FormatterCollection $formatters) {
-                    $save_format = $config->get('date_save_format', '');
-                    $date_formatter = $save_format
-                        ? new Formatter\DateMapper($save_format, 'U')
-                        : new Formatter\Timestamp();
-                    $formatters->prepend($date_formatter);
-                }
-            )->add_child_component(
-                $this->date_format,
-                StringComparisonSpecification::equal(self::TYPE)
-            )
-            ->add_child_component(
-                $this->date_save_format,
-                StringComparisonSpecification::equal(self::TYPE)
-            );
+        $builder->add_option(self::TYPE, __('Number of Fields', 'codepress-admin-columns'), 'multiple');
     }
 }
