c420dff7aba621aa6a6c552c610760b1ff2df723
David Mosterd <david@codepress.nl>
2025-12-01 15:44:48 +0100

    collection experiment on priorities

--- classes/Setting/ComponentCollection.php
+++ classes/Setting/ComponentCollection.php
@@ -11,6 +11,13 @@ final class ComponentCollection extends Collection
 
     private int $added = 0;
 
+    private const ADDED_SEPARATOR = '.';
+
+    /**
+     * @var string[]
+     */
+    private array $priorities = [];
+
     public function __construct(array $data = [], int $priority = 10)
     {
         foreach ($data as $component) {
@@ -18,14 +25,26 @@ final class ComponentCollection extends Collection
         }
     }
 
+    private function get_priority_key(int $priority): string
+    {
+        return $priority . self::ADDED_SEPARATOR . $this->added++;
+    }
+
+    private function get_priority_from_key(string $priority_key): int
+    {
+        return (int)strstr($priority_key, self::ADDED_SEPARATOR, true);
+    }
+
     /**
      * Priority is taken into account when iterating. E.g. 1 comes before 5.
      */
     public function add(Component $component, int $priority = 10): ComponentCollection
     {
-        $this->data[$priority . '.' . $this->added++] = $component;
+        $this->data[$this->get_priority_key($priority)] = $component;
 
         ksort($this->data, SORT_NATURAL);
+
+        $this->priorities = array_keys($this->data);
 
         return $this;
     }
@@ -35,12 +54,21 @@ final class ComponentCollection extends Collection
         return current($this->data);
     }
 
+    public function current_priority(): int
+    {
+        return $this->get_priority_from_key($this->priorities[$this->index]);
+    }
+
     public function merge(ComponentCollection $collection): ComponentCollection
     {
-        $merged = new ComponentCollection($this->data);
+        $merged = new ComponentCollection();
+
+        foreach( $this as $component ) {
+            $merged->add($component, $this->current_priority() );
+        }
 
         foreach ($collection as $component) {
-            $merged->add($component);
+            $merged->add($component, $collection->current_priority());
         }
 
         return $merged;
