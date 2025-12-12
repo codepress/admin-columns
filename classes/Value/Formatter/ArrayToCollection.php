07e377e6b3d5836ff1e42c4bb929b8c9fed79e86
Tobias Schutter <tobias@codepress.nl>
2025-12-12 14:54:32 +0100

    Update ArrayToCollection.php

--- classes/Value/Formatter/ArrayToCollection.php
+++ classes/Value/Formatter/ArrayToCollection.php
@@ -12,12 +12,23 @@ use AC\Type\ValueCollection;
 class ArrayToCollection implements Formatter
 {
 
+    private bool $convert_to_array;
+
+    public function __construct(bool $convert_to_array = true)
+    {
+        $this->convert_to_array = $convert_to_array;
+    }
+
     public function format(Value $value): ValueCollection
     {
         $array = $value->get_value();
 
-        if ( ! is_array($array) || ! $array) {
-            throw new ValueNotFoundException('No array found. ID . ' . $value->get_id());
+        if (is_scalar($array) && $this->convert_to_array) {
+            $array = [$array];
+        }
+
+        if ( ! is_array($array)) {
+            throw new ValueNotFoundException('Invalid value type. Expected array, got: ' . gettype($array));
         }
 
         $collection = new ValueCollection($value->get_id());
