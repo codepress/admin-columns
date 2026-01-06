3820d3023e25ac5d79a519bf073bb66a6496232b
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2026-01-06 13:07:49 +0100

    Update admin.php

--- templates/message/notice/admin.php
+++ templates/message/notice/admin.php
@@ -6,6 +6,6 @@ if ( ! defined('ABSPATH')) {
 
 $class = $this->type . ' ' . $this->id;
 ?>
-<div class="ac-notice notice ac-message ac-message--styled ac-message--icon <?= $class ?>">
+<div class="ac-notice notice ac-message ac-message--styled ac-message--icon <?= $class ?> notice-<?= $class ?>">
 	<p><?= $this->message ?></p>
 </div>
