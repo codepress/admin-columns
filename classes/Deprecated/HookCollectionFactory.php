55168ded77d72d308abed82cdfaec89f8ad5dccc
Tobias Schutter <tobias@codepress.nl>
2026-02-10 15:00:47 +0100

    Update HookCollectionFactory.php

--- classes/Deprecated/HookCollectionFactory.php
+++ classes/Deprecated/HookCollectionFactory.php
@@ -100,7 +100,7 @@ class HookCollectionFactory
         }
 
         $pro_actions = [
-            'acp/column_types'                  => 'ac/column/types/pro',
+            'acp/column_types'                  => 'ac/column/types',
             'acp/acf/after_get_field_options'   => 'ac/acf/after_get_field_options',
             'acp/acf/before_get_field_options'  => 'ac/acf/before_get_field_options',
             'acp/admin/settings/hide_on_screen' => null,
@@ -109,6 +109,7 @@ class HookCollectionFactory
             'acp/editing/saved'                 => 'ac/editing/saved',
             'acp/editing/before_save'           => 'ac/editing/before_save',
             'acp/admin/settings/table_elements' => 'ac/admin/settings/table_elements',
+            'ac/column_groups'                  => 'ac/column/groups',
         ];
 
         foreach ($pro_actions as $old => $replacement) {
