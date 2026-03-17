2246e855a5a109f5aa8753679b62cdc925848316
Tobias Schutter <tobias@codepress.nl>
2026-03-17 10:51:20 +0100

    WordCount before and after

--- classes/ColumnFactory/Post/WordCountFactory.php
+++ classes/ColumnFactory/Post/WordCountFactory.php
@@ -7,10 +7,24 @@ namespace AC\ColumnFactory\Post;
 use AC;
 use AC\Column\BaseColumnFactory;
 use AC\FormatterCollection;
+use AC\Setting\ComponentCollection;
+use AC\Setting\ComponentFactory\BeforeAfter;
 use AC\Setting\Config;
+use AC\Setting\DefaultSettingsBuilder;
 
 class WordCountFactory extends BaseColumnFactory
 {
+
+    private BeforeAfter $before_after_factory;
+
+    public function __construct(
+        DefaultSettingsBuilder $default_settings_builder,
+        BeforeAfter $before_after_factory
+    ) {
+        parent::__construct($default_settings_builder);
+
+        $this->before_after_factory = $before_after_factory;
+    }
 
     public function get_column_type(): string
     {
@@ -26,10 +40,17 @@ class WordCountFactory extends BaseColumnFactory
     {
         $formatters = parent::get_formatters($config);
 
-        $formatters->add(new AC\Formatter\Post\PostContent());
-        $formatters->add(new AC\Formatter\WordCount());
+        $formatters->prepend(new AC\Formatter\WordCount());
+        $formatters->prepend(new AC\Formatter\Post\PostContent());
 
         return $formatters;
+    }
+
+    protected function get_settings(Config $config): ComponentCollection
+    {
+        return new ComponentCollection([
+            $this->before_after_factory->create($config),
+        ]);
     }
 
 }
