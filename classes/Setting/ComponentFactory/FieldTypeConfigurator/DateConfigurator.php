6bd3d696c0d556000af219fb9333fd7f1373168f
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2026-04-07 11:17:14 +0200

    Update DateConfigurator.php

--- classes/Setting/ComponentFactory/FieldTypeConfigurator/DateConfigurator.php
+++ classes/Setting/ComponentFactory/FieldTypeConfigurator/DateConfigurator.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace AC\Setting\ComponentFactory\FieldTypeConfigurator;
 
 use AC\Expression\StringComparisonSpecification;
+use AC\Formatter;
 use AC\Setting;
 use AC\Setting\ComponentFactory;
 use AC\Setting\ComponentFactory\FieldTypeFactoryBuilder;
-use AC\Value\Formatter;
 
 class DateConfigurator implements FieldTypeConfigurator
 {
@@ -35,8 +35,13 @@ class DateConfigurator implements FieldTypeConfigurator
                 self::TYPE,
                 function (Setting\Config $config, Setting\FormatterCollection $formatters) {
                     $save_format = $config->get('date_save_format', '');
+
+                    if ($save_format === ComponentFactory\DateSaveFormat::FORMAT_AUTO) {
+                        $formatters->prepend(new Formatter\Date\Timestamp());
+                    }
+
                     $date_formatter = $save_format
-                        ? new Formatter\DateMapper($save_format, 'U')
+                        ? new Formatter\Date\DateFormat('U', $save_format)
                         : new Formatter\Date\Timestamp();
                     $formatters->prepend($date_formatter);
                 }
