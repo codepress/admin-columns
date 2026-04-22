0b18955f3d5de3b85b56a0d9fea00b59fe1eb8ea
Tobias Schutter <tobias@codepress.nl>
2026-04-22 13:01:46 +0200

    refactor: use AttachmentsByParent helper in Attachments formatter

--- classes/Formatter/Post/Attachments.php
+++ classes/Formatter/Post/Attachments.php
@@ -6,11 +6,19 @@ namespace AC\Formatter\Post;
 
 use AC\Exception\ValueNotFoundException;
 use AC\Formatter;
+use AC\Helper\AttachmentsByParent;
 use AC\Type\Value;
 use AC\Type\ValueCollection;
 
 class Attachments implements Formatter
 {
+
+    private AttachmentsByParent $attachments_by_parent;
+
+    public function __construct(?AttachmentsByParent $attachments_by_parent = null)
+    {
+        $this->attachments_by_parent = $attachments_by_parent ?? new AttachmentsByParent();
+    }
 
     public function format(Value $value): ValueCollection
     {
@@ -20,35 +28,7 @@ class Attachments implements Formatter
             throw new ValueNotFoundException('Parent ID is required');
         }
 
-        $attachment_ids = get_posts([
-            'post_type'      => 'attachment',
-            'posts_per_page' => -1,
-            'post_status'    => null,
-            'post_parent'    => $parent_id,
-            'fields'         => 'ids',
-        ]);
-
-        return ValueCollection::from_ids($parent_id, $this->group_by_extension($attachment_ids));
-    }
-
-    /**
-     * Group attachment IDs by file extension, preserving the order of first appearance.
-     *
-     * @param int[] $ids
-     * @return int[]
-     */
-    private function group_by_extension(array $ids): array
-    {
-        $groups = [];
-
-        foreach ($ids as $id) {
-            $file = get_attached_file($id);
-            $ext  = $file ? strtolower((string)pathinfo($file, PATHINFO_EXTENSION)) : '';
-
-            $groups[$ext][] = $id;
-        }
-
-        return array_merge(...array_values($groups));
+        return ValueCollection::from_ids($parent_id, $this->attachments_by_parent->find($parent_id));
     }
 
 }
