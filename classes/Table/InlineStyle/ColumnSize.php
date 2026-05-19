d9b58dfc0ed659e05e110d58e210dcaa03e143dd
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2026-05-19 13:39:45 +0200

    BEtter support for column resizer

--- classes/Table/InlineStyle/ColumnSize.php
+++ classes/Table/InlineStyle/ColumnSize.php
@@ -40,26 +40,36 @@ class ColumnSize
             $list_width = $this->list_storage->get($column);
             $user_width = $this->user_storage->get($list_screen->get_id(), $column_id);
 
-            if ( ! $list_width && ! $user_width) {
-                continue;
-            }
+            $html .= $this->render_base_style($table_id, $column_id, $list_width);
 
-            $html .= $this->render_style($table_id, $column_id, $list_width, $user_width);
+            if ($user_width) {
+                $html .= $this->render_user_style($table_id, $column_id, $user_width);
+            }
         }
 
         return $html;
     }
 
-    private function render_style(TableId $table_id, ColumnId $column_id, ?ColumnWidth $list_width, ?ColumnWidth $user_width): string
+    private function render_base_style(TableId $table_id, ColumnId $column_id, ?ColumnWidth $list_width): string
     {
         $view = new View([
             'table_id'   => (string)$table_id,
             'column_id'  => (string)$column_id,
             'list_width' => $this->format_width($list_width),
-            'user_width' => $this->format_width($user_width),
         ]);
 
         return $view->set_template('table/column-size')->render();
+    }
+
+    private function render_user_style(TableId $table_id, ColumnId $column_id, ColumnWidth $user_width): string
+    {
+        $view = new View([
+            'table_id'   => (string)$table_id,
+            'column_id'  => (string)$column_id,
+            'user_width' => $this->format_width($user_width),
+        ]);
+
+        return $view->set_template('table/column-size-user')->render();
     }
 
     private function format_width(?ColumnWidth $width): ?string
--- templates/table/column-size.php
+++ templates/table/column-size.php
@@ -3,12 +3,13 @@
 declare(strict_types=1);
 
 /**
- * @var \AC\View    $this
+ * @var View        $this
  * @var string      $table_id
  * @var string      $column_id
  * @var string|null $list_width
- * @var string|null $user_width
  */
+
+use AC\View;
 
 $table_id = esc_attr($this->table_id);
 $column_id = esc_attr($this->column_id);
@@ -17,17 +18,14 @@ $style_id = esc_attr(sprintf('ac-column-size-%s', $this->column_id));
 $var_list = sprintf('--ac-col-%s-width', $column_id);
 $var_user = $var_list . '-user';
 $value = sprintf('var(%s, var(%s))', $var_user, $var_list);
-
-$assignments = [];
-if ($this->list_width !== null) {
-    $assignments[] = sprintf('%s: %s;', $var_list, esc_attr($this->list_width));
-}
-if ($this->user_width !== null) {
-    $assignments[] = sprintf('%s: %s;', $var_user, esc_attr($this->user_width));
-}
 ?>
 <style id="<?= $style_id ?>">
-	.ac-<?= $table_id ?> { <?= implode(' ', $assignments) ?> }
+	<?php if ($this->list_width !== null): ?>
+	.ac-<?= $table_id ?> {
+	<?= $var_list ?>: <?= esc_attr($this->list_width) ?>;
+	}
+
+	<?php endif; ?>
 
 	@media screen and (min-width: 783px) {
 		.ac-<?= $table_id ?> .wrap table th.column-<?= $column_id ?>,
