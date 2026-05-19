2a9787a66e91d3b7619f2261fbc52b28c9727d3d
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2026-05-19 11:47:16 +0200

    More readable styles for Width

--- classes/Table/InlineStyle/ColumnSize.php
+++ classes/Table/InlineStyle/ColumnSize.php
@@ -24,41 +24,6 @@ class ColumnSize
         $this->user_storage = $user_storage;
     }
 
-    private function render_style(TableId $table_id, ColumnId $column_id, ColumnWidth $column_width, $type)
-    {
-        $css_width = $column_width->get_value() . $column_width->get_unit();
-
-        $css = sprintf(
-            '.ac-%1$s .wrap table th.column-%2$s, .ac-%1$s .wrap table td.column-%2$s   { width: %3$s !important; }',
-            esc_attr((string)$table_id),
-            esc_attr((string)$column_id),
-            $css_width
-        );
-
-        $css .= sprintf(
-            'body.acp-overflow-table.ac-%1$s .wrap th.column-%2$s, body.acp-overflow-table.ac-%1$s .wrap td.column-%2$s { min-width: %3$s !important; max-width: %3$s !important }',
-            esc_attr((string)$table_id),
-            esc_attr((string)$column_id),
-            $css_width
-        );
-
-        $id = sprintf(
-            'ac-column-size-%s-%s',
-            $type,
-            $column_id
-        );
-
-        ob_start();
-        ?>
-		<style id="<?= $id ?>">
-			@media screen and (min-width: 783px) {
-			<?= $css ?>
-			}
-		</style>
-        <?php
-        return ob_get_clean();
-    }
-
     public function render(ListScreen $list_screen): string
     {
         // auto-width is not supported with wrapping
@@ -67,22 +32,58 @@ class ColumnSize
         }
 
         $html = '';
+        $table_id = $list_screen->get_table_id();
 
         foreach ($list_screen->get_columns() as $column) {
-            $width = $this->list_storage->get($column);
+            $column_id = $column->get_id();
 
-            if ($width) {
-                $html .= $this->render_style($list_screen->get_table_id(), $column->get_id(), $width, 'list');
+            $list_width = $this->list_storage->get($column);
+            if ($list_width) {
+                $html .= $this->render_style($table_id, $column_id, $list_width, 'list');
             }
 
-            $width = $this->user_storage->get($list_screen->get_id(), $column->get_id());
-
-            if ($width) {
-                $html .= $this->render_style($list_screen->get_table_id(), $column->get_id(), $width, 'user');
+            $user_width = $this->user_storage->get($list_screen->get_id(), $column_id);
+            if ($user_width) {
+                $html .= $this->render_style($table_id, $column_id, $user_width, 'user');
             }
         }
 
         return $html;
+    }
+
+    private function render_style(TableId $table_id, ColumnId $column_id, ColumnWidth $column_width, string $type): string
+    {
+        $id = sprintf('ac-column-size-%s-%s', $type, $column_id);
+        $rules = $this->build_css_rules($table_id, $column_id, $column_width);
+
+        return sprintf(
+            '<style id="%s">@media screen and (min-width: 783px) { %s }</style>',
+            esc_attr($id),
+            $rules
+        );
+    }
+
+    private function build_css_rules(TableId $table_id, ColumnId $column_id, ColumnWidth $column_width): string
+    {
+        $table = esc_attr((string)$table_id);
+        $column = esc_attr((string)$column_id);
+        $width = $column_width->get_value() . $column_width->get_unit();
+
+        $width_rule = sprintf(
+            '.ac-%1$s .wrap table th.column-%2$s, .ac-%1$s .wrap table td.column-%2$s { width: %3$s !important; }',
+            $table,
+            $column,
+            $width
+        );
+
+        $overflow_rule = sprintf(
+            'body.acp-overflow-table.ac-%1$s .wrap th.column-%2$s, body.acp-overflow-table.ac-%1$s .wrap td.column-%2$s { min-width: %3$s !important; max-width: %3$s !important; }',
+            $table,
+            $column,
+            $width
+        );
+
+        return $width_rule . ' ' . $overflow_rule;
     }
 
 }
