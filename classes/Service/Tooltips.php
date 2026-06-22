b13aab736c997d544cebc88c10a484d31adcdeae
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2026-06-22 09:09:13 +0200

    Set correct path for images in view

--- classes/Service/Tooltips.php
+++ classes/Service/Tooltips.php
@@ -4,11 +4,19 @@ declare(strict_types=1);
 
 namespace AC\Service;
 
+use AC\AdminColumns;
 use AC\Registerable;
 use AC\View;
 
 class Tooltips implements Registerable
 {
+    private string $url;
+
+    public function __construct(AdminColumns $plugin)
+    {
+        $this->url = rtrim($plugin->get_url(), '/');
+    }
+
     public function register(): void
     {
         add_filter('ac/page/columns/render', [$this, 'render'], 10, 2);
@@ -16,7 +24,7 @@ class Tooltips implements Registerable
 
     private function create_view(string $slug): string
     {
-        return (new View())->set_template('tooltip/' . $slug)->render();
+        return (new View(['url' => $this->url]))->set_template('tooltip/' . $slug)->render();
     }
 
     public function render(string $html): string
