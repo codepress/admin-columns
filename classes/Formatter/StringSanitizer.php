5a97eb64a8bbf99c6828700ad10e68d4a970d2d2
Stefan van den Dungen Gronovius <dungengronovius@gmail.com>
2026-07-06 09:17:39 +0200

    Strip more space types

--- classes/Formatter/StringSanitizer.php
+++ classes/Formatter/StringSanitizer.php
@@ -13,9 +13,11 @@ class StringSanitizer implements Formatter
     public function format(Value $value): Value
     {
         $formatter = Aggregate::from_array([
-            new StripTags(),
             (new PregReplace())
                 ->replace_br()
+                ->replace_non_breaking_space(),
+            new StripTags(),
+            (new PregReplace())
                 ->replace_tabs()
                 ->replace_new_line()
                 ->replace_multiple_spaces(),
