ff891e5bd3c6b2796c6cf1ead277d0f16ff29e0b
Tobias Schutter <tobias@codepress.nl>
2026-07-14 13:54:17 +0200

    Render grouped More dropdown in admin menu template

--- templates/admin/menu.php
+++ templates/admin/menu.php
@@ -8,6 +8,17 @@ use AC\Admin\Type\MenuItem;
  * @var MenuItem[] $items
  */
 $items = $this->menu_items;
+$current = (string)$this->current;
+
+$primary_items = array_values(
+    array_filter($items, static fn(MenuItem $item): bool => $item->get_group() === MenuItem::GROUP_PRIMARY)
+);
+$more_items = array_values(
+    array_filter($items, static fn(MenuItem $item): bool => $item->get_group() === MenuItem::GROUP_MORE)
+);
+
+$more_slugs = array_map(static fn(MenuItem $item): string => $item->get_slug(), $more_items);
+$more_active = in_array($current, $more_slugs, true);
 
 $first = $items[0];
 ?>
@@ -21,17 +32,51 @@ $first = $items[0];
 	<div class="acu-flex acu-flex-grow">
 		<ul class="acu-flex ac-admin-nav acu-gap-2 acu-flex-wrap acu-flex-grow">
             <?php
-            foreach ($items as $item) : ?>
+            foreach ($primary_items as $item) : ?>
 				<li class="ac-admin-nav__item <?= esc_attr($item->get_class()); ?>">
-					<a href="<?= esc_url(
-                        $item->get_url()
-                    ) ?>" class="acu-text-[#fff] acu-inline-block ac-admin-nav__link"
-                        <?= $item->get_target() ? sprintf(' target="%s"', $item->get_target()) : '' ?>>
+					<a href="<?= esc_url($item->get_url()) ?>" class="acu-text-[#fff] acu-inline-block ac-admin-nav__link"
+                        <?= $item->get_target() ? sprintf(' target="%s"', esc_attr($item->get_target())) : '' ?>
+                        <?= $current === $item->get_slug() ? ' aria-current="page"' : '' ?>>
                         <?= $item->get_label() ?>
 					</a>
 				</li>
             <?php
             endforeach; ?>
+
+            <?php
+            if ($more_items) : ?>
+				<li class="ac-admin-nav__item ac-admin-nav__item--more <?= $more_active ? '-active' : '' ?>">
+					<button type="button"
+							id="ac-admin-nav-more-trigger"
+							class="acu-text-[#fff] ac-admin-nav__link ac-admin-nav__trigger"
+							aria-expanded="false"
+							aria-haspopup="true"
+							aria-controls="ac-admin-nav-more">
+                        <?php
+                        esc_html_e('More', 'codepress-admin-columns'); ?>
+						<span class="ac-admin-nav__chevron" aria-hidden="true"></span>
+					</button>
+					<ul id="ac-admin-nav-more" class="ac-admin-nav__dropdown" hidden>
+                        <?php
+                        foreach ($more_items as $item) : ?>
+                            <?php
+                            if ($item->has_separator_before()) : ?>
+								<li class="ac-admin-nav__separator" role="separator"></li>
+                            <?php
+                            endif; ?>
+							<li class="ac-admin-nav__dropdown-item <?= esc_attr($item->get_class()); ?>">
+								<a href="<?= esc_url($item->get_url()) ?>" class="ac-admin-nav__dropdown-link"
+                                    <?= $item->get_target() ? sprintf(' target="%s"', esc_attr($item->get_target())) : '' ?>
+                                    <?= $current === $item->get_slug() ? ' aria-current="page"' : '' ?>>
+                                    <?= $item->get_label() ?>
+								</a>
+							</li>
+                        <?php
+                        endforeach; ?>
+					</ul>
+				</li>
+            <?php
+            endif; ?>
 		</ul>
 	</div>
 </header>
